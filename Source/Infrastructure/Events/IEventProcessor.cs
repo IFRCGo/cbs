b67fb72a86d43ead88f10bf61777d21554d5ebe1
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-15 17:27:25 +0200

    Event processor marker interface

---
+++ Source/Infrastructure/Events/IEventProcessor.cs
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+namespace Events
+{
+    /// <summary>
+    /// Marker interface for processors of <see cref="IEvent"/>
+    /// </summary>
+    public interface IEventProcessor
+    {
+    }
+}
