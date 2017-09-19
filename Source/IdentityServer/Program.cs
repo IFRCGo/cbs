370604239418f1ad7a8af2791c54efffc586f290
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-19 20:28:02 +0200

    Starting the IdentityServer project

---
+++ Source/IdentityServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
