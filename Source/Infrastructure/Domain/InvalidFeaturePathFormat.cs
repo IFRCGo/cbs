d79763ebf079fb134359ca99ac154109fb863e7c
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-26 09:58:15 +0200

    Introducing Feature as a concept

---
+++ Source/Infrastructure/Domain/InvalidFeaturePathFormat.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Domain
+{
+    /// <summary>
+    /// Exception that gets thrown when the format of a feature path is invalid
+    /// </summary>
+    public class InvalidFeaturePathFormat : ArgumentException
+    {
+        /// <summary>
+        /// Initialize a new instance of <see cref="InvalidFeaturePathFormat"/>
+        /// </summary>
+        /// <param name="path">The path that is invalid</param>
+        public InvalidFeaturePathFormat(string path) : base($"Format of the '{path}' is invalid - correct format is 'some/path/in/boundedContext'")
+        {
+
+        }
+    }
+}
