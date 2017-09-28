3b2baf2fa6173ca9de52b449b65a79cf5470128f
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-28 19:10:06 +0200

    Started on the module for events infrastructure

---
+++ Source/Infrastructure/Events/EventModule.cs
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using Autofac;
+
+namespace Events
+{
+    /// <summary>
+    /// Represents the Autofac module for dealing with events infrastructure
+    /// </summary>
+    public class EventModule : Module
+    {
+        /// <inheritdoc/>
+        protected override void Load(ContainerBuilder builder)
+        {
+            var i=0;
+            i++;
+
+        }
+    }
+}
