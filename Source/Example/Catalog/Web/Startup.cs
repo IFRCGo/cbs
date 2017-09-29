2ada38829c338931db4871b8e3dfa485029faa7b
Einar Ingebrigtsen <einar@dolittle.com>
2017-09-29 17:25:24 +0200

    Cleanup

--- Source/Example/Catalog/Web/Startup.cs
+++ Source/Example/Catalog/Web/Startup.cs
@@ -8,7 +8,10 @@ namespace Web
 {
     public class Startup : Infrastructure.AspNet.Startup
     {
-        public Startup(ILoggerFactory loggerFactory, IHostingEnvironment env, IConfiguration configuration) : base(loggerFactory, env, configuration)
+        public Startup(
+            ILoggerFactory loggerFactory, 
+            IHostingEnvironment env, 
+            IConfiguration configuration) : base(loggerFactory, env, configuration)
         {
         }
 
