166cf8827aa8b73aae9953d406929dad4a4e342d
Maren Fredbo <mfredbo@gmail.com>
2017-09-30 17:51:13 +0200

    Added new event @roarfred

---
+++ Source/Admin/Events/ProjectUpdated.cs
@@ -0,0 +1,17 @@
+// /*---------------------------------------------------------------------------------------------
+//  *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+//  *  Licensed under the MIT License. See LICENSE in the project root for license information.
+//  *--------------------------------------------------------------------------------------------*/
+
+using System;
+using Infrastructure.Events;
+
+namespace Events
+{
+    public class ProjectUpdated : IEvent
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
