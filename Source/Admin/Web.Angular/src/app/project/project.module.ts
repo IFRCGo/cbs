7c24913f7f26d356cd4a7b647b282ac28ec80d01
Anders Austad <anderaus@gmail.com>
2017-09-30 13:42:35 +0200

    Add projectlist component

--- Source/Admin/Web.Angular/src/app/project/project.module.ts
+++ Source/Admin/Web.Angular/src/app/project/project.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectRouting } from './project-routing.module';
 import { AddProjectComponent } from './add-project/add-project.component';
+import { ProjectlistComponent } from './projectlist/projectlist.component';
 
 @NgModule({
     imports: [
         CommonModule,
         ProjectRouting
     ],
-    declarations: [AddProjectComponent]
+    declarations: [
+        AddProjectComponent, 
+        ProjectlistComponent
+    ]
 })
 
 export class ProjectModule { }
