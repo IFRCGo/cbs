96e31d4223e943a1dab922f66a56f9404aba9ffd
Pål Wesenberg <p.wesenberg@gmail.com>
2017-09-30 13:01:21 +0200

    Update user event

--- Source/Alert/Events/External/UserUpdatedEvent.cs
+++ Source/Alert/Events/External/UserUpdatedEvent.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 using Infrastructure.Events;
 
-namespace Events.External
+namespace Events
 {
-    public class UserCreatedEvent : IEvent
+    public class UserUpdatedEvent : IEvent
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
--- Source/Alert/Read/UserEventProcessor.cs
+++ Source/Alert/Read/UserEventProcessor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Events;
+using Events.External;
 
 namespace Read
 {
@@ -15,6 +16,33 @@ namespace Read
         }
 
         public void Process(UserCreatedEvent @event)
+        {
+            var user = _users.GetById(@event.Id);
+            if (user == null)
+            {
+                user = new User()
+                {
+                    Id = @event.Id,
+                    Name = @event.Name,
+                    PhoneNumber = @event.PhoneNumber,
+                    Email = @event.Email,
+                    Geo = @event.Geo,
+                    RecipientType = @event.UserType
+                };
+            }
+            else
+            {
+                user.Id = @event.Id;
+                user.Name = @event.Name;
+                user.PhoneNumber = @event.PhoneNumber;
+                user.Email = @event.Email;
+                user.Geo = @event.Geo;
+                user.RecipientType = @event.UserType;
+            }
+            _users.Save(user);
+        }
+
+        public void Process(UserUpdatedEvent @event)
         {
             var user = _users.GetById(@event.Id);
             if (user == null)
