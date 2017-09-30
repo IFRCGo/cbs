8efadd727fd835684d9b6e72286a93ba1ccb25b5
Pål Wesenberg <p.wesenberg@gmail.com>
2017-09-30 14:05:26 +0200

    Disease

--- Source/Alert/Read/DiseaseEventProcessor.cs
+++ Source/Alert/Read/DiseaseEventProcessor.cs
@@ -1,11 +1,38 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Events.External;
 
 namespace Read
 {
-    public class DiseaseEventProcessor
+    public class DiseaseEventProcessor : Infrastructure.Events.IEventProcessor
     {
+        private readonly IDiseases _diseases;
 
+        public DiseaseEventProcessor(IDiseases diseases)
+        {
+            _diseases = diseases;
+        }
+
+        public void Process(DiseaseCreatedEvent @event)
+        {
+            var user = _diseases.GetById(@event.Id);
+            if (user == null)
+            {
+                user = new Disease()
+                {
+                    Id = @event.Id,
+                    Name = @event.Name,
+                    Code = @event.Code
+                };
+            }
+            else
+            {
+                user.Id = @event.Id;
+                user.Name = @event.Name;
+                user.Code = @event.Code;
+            }
+            _diseases.Save(user);
+        }
     }
 }
