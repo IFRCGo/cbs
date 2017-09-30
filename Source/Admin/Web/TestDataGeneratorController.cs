526481387a1d3b5b1f0e95c8364066a32c7c0341
Roar Fredriksen <roarfred@gmail.com>
2017-09-30 22:20:22 +0200

    Controller to create test data

---
+++ Source/Admin/Web/TestDataGeneratorController.cs
@@ -0,0 +1,47 @@
+using Events;
+using Infrastructure.Events;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web
+{
+
+    [Route("api/testdatagenerator")]
+    public class TestDataGeneratorController
+    {
+        private IEventEmitter _eventEmitter;
+        private Guid[] _nationalSocietyIds = new Guid[] { new Guid("917b98f2-1435-4b0d-88f1-f177a926e374"), new Guid("e13b1996-11a5-4d55-a04c-0cc1962cb0a9"), new Guid("267f83bf-fbe0-4fa7-a356-15fe478348b8"), new Guid("099a5bae-c8cd-472c-9a98-dc15c770598c"), new Guid("4405540c-78bc-4484-ae06-e00be468770c"), new Guid("53a5712c-663a-41f8-bbb1-bc4fd834646e") };
+        private Guid[] _userIds = new Guid[] { new Guid("917b98f2-1435-4b0d-88f1-f177a926e374"), new Guid("e13b1996-11a5-4d55-a04c-0cc1962cb0a9"), new Guid("267f83bf-fbe0-4fa7-a356-15fe478348b8"), new Guid("099a5bae-c8cd-472c-9a98-dc15c770598c"), new Guid("4405540c-78bc-4484-ae06-e00be468770c"), new Guid("53a5712c-663a-41f8-bbb1-bc4fd834646e"), new Guid("9835cf5d-0b43-463e-b8f7-3bc94b911679"), new Guid("38ec7f61-aa60-4ed1-8b08-41783423e1e8"), new Guid("70736cc9-1194-44ac-a5e5-fb4b7f165b09"), new Guid("c5d92ded-e42f-439f-a559-63d353b73223") };
+
+        public TestDataGeneratorController(IEventEmitter eventEmitter)
+        {
+            _eventEmitter = eventEmitter;
+        }
+
+        [HttpGet("all")]
+        public void CreateAll()
+        {
+            CreateNationalSociety();
+            CreateUsers();
+        }
+
+        [HttpGet("nationalsocieties")]
+        public void CreateNationalSociety()
+        {
+            int i = 1;
+            foreach (var id in _nationalSocietyIds)
+                _eventEmitter.Emit("NationalSecurity", new NationalSocietyCreated() { Id = id, Name = $"National Sociity #{i++}" });
+        }
+
+        [HttpGet("users")]
+        public void CreateUsers()
+        {
+            int i = 0;
+            foreach (var id in _userIds)
+                _eventEmitter.Emit("User", new UserCreated() { Id = id, Firstname = $"User {i + 1}", Lastname = "Doe", NationalSocietyId = _nationalSocietyIds[i++ % _nationalSocietyIds.Length], Country = "Norway" });
+        }
+    }
+}
