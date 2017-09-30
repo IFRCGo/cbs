1d6c706ca4dc3223d0e89cc6bbe5fb7d00ea6afa
Baulo, Oleg <oleg.baulo@inmeta.no>
2017-09-30 12:53:27 +0200

    Added Threshold event

--- Source/Alert/Events/External/ProjectDiseaseThresholdChanged.cs
+++ Source/Alert/Events/External/ProjectDiseaseThresholdChanged.cs
@@ -7,6 +7,21 @@ namespace Events.External
 {
     public class ProjectDiseaseThresholdChanged : IEvent
     {
-        
+        /// <summary>
+        /// The id of project wo which threshold is related to.
+        /// </summary>
+        public Guid ProjectId { get; set; }
+        /// <summary>
+        /// The id of disease wo which threshold is related to.
+        /// </summary>
+        public Guid DiseaseId { get; set; }
+        /// <summary>
+        /// Number of cases per alert period that will trigger an alert.
+        /// </summary>
+        public int Threshold { get; set; }
+        /// <summary>
+        /// Period on which threshold is checked.
+        /// </summary>
+        public TimeSpan ThresholdPeriod { get; set; }
     }
 }
