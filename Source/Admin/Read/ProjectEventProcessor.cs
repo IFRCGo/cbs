74e1a3052ad4761345eac6e9c708627496b7be50
Roar Fredriksen <roarfred@gmail.com>
2017-09-30 13:13:16 +0200

    Implemented event processor for Project

--- Source/Admin/Read/ProjectEventProcessor.cs
+++ Source/Admin/Read/ProjectEventProcessor.cs
@@ -1,3 +1,4 @@
+using Events;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,14 +8,17 @@ namespace Read
     public class ProjectEventProcessor : Infrastructure.Events.IEventProcessor
     {
         readonly IProjects _projects;
+
         public ProjectEventProcessor(IProjects projects)
         {
             _projects = projects;
         }
 
-        public void Process()
+        public void Process(CreatedProject @event)
         {
-
+            var project = _projects.GetById(@event.Id);
+            project.Name = @event.Name;
+            _projects.Save(project);
         }
     }
 }
