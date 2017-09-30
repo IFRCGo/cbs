de135abd92ec68d0a89a1909186d0dd09eff6501
Baulo, Oleg <oleg.baulo@inmeta.no>
2017-09-30 12:39:28 +0200

    created external events folder

---
+++ Source/Alert/Events/External/ProjectDiseaseThresholdChanged.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Infrastructure.Events;
+
+namespace Events.External
+{
+    public class ProjectDiseaseThresholdChanged : IEvent
+    {
+        
+    }
+}
