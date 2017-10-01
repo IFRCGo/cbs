8331654ab40f421e8cc0e2a8d79c29593961d183
Baulo, Oleg <oleg.baulo@inmeta.no>
2017-10-01 12:12:20 +0200

    Removed service interface from domain

--- Source/Alert/Policies/AlertFeedbackService.cs
+++ Source/Alert/Policies/AlertFeedbackService.cs
@@ -25,15 +25,17 @@ namespace Domain
             _eventEmitter = eventEmitter;
             _messageTemplateService = messageTemplateService;
         }
+
         public void SendFeedbackToDataCollecorsAndVerifiers(IEnumerable<CaseReport> latestReports)
         {
             var dataCollectors = _dataCollectors.Get(latestReports.Select(o => o.DataCollectorId).ToArray());
             var dataVerifiers = dataCollectors.GroupBy(o => o.DataVerifier.UserId).Select(o => o.First().DataVerifier).ToArray();
+            var healthRisk = latestReports.First().HealthRiskId;
 
             // send sms to all data verifiers
             foreach (DataVerifier dataVerifier in dataVerifiers)
             {
-                string text = "";
+                string text = _messageTemplateService.ComposeMessage("AlertRaisedFeedbackToDataCollectors", healthRisk);
                 _smsSendingService.SendSMS(new [] { dataVerifier.Phone }, "");
                 _eventEmitter.Emit(Feature, new SmsSentEvent() { RecipientName = dataVerifier.Name, RecipientPhoneNumber = dataVerifier.Phone, SmsText = text});
             }
@@ -41,10 +43,26 @@ namespace Domain
             // send sms to all data collecors
             foreach (DataCollector dataCollector in dataCollectors)
             {
-                string text = "";
+                string text = _messageTemplateService.ComposeMessage("AlertRaisedFeedbackToDataVerifiers", healthRisk);
                 _smsSendingService.SendSMS(new[] { dataCollector.Phone }, "");
                 _eventEmitter.Emit(Feature, new SmsSentEvent() { RecipientName = dataCollector.Name, RecipientPhoneNumber = dataCollector.Phone, SmsText = text });
             }
         }
     }
+
+
+    /*
+     * 
+     * Trigger escalation due to threshold 	
+     * message to data collectors triggered
+     * "Your [event] report(s) has triggered an alert. [Escalation UID], phone number: [phone number] will contact you for follow up. You need to [Key action]." 	#54
+
+Alert trigger by others 	"An alert for [event] has been triggered for [location]. [Escalation UID], phone number: [phone number] will contact you for follow up. Please [Key action]."
+to all neighbours of all that reported
+when it is confirmed
+
+    message to data verifier 
+    [Health Risk] has triggered an alert. Please follow up with: [FullName] [Phone] (repeating). (Requires form attachment).
+    Full name phone of all data collectors that send reports
+     * */
 }
