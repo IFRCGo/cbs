7c386cd586e966da0145f529c31de1896d79c114
Roar Fredriksen <roarfred@gmail.com>
2017-10-19 18:44:55 +0200

    Empty test project created #233
    There's a failing test in here, will it be caught by appveyor?

---
+++ Source/VolunteerReporting/Tests/Web.Tests/TestClass.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace Web.Tests
+{
+    public class TestClass
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
