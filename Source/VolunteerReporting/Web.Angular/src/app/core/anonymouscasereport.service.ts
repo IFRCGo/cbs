897f1a086673ee0da0414ee0809e48f040d1d6e1
Roar Fredriksen <roarfred@gmail.com>
2017-10-20 19:18:52 +0200

    Separated model and api for case reports
    Anonymous Case Reports now has it's own API, ref #243

---
+++ Source/VolunteerReporting/Web.Angular/src/app/core/anonymouscasereport.service.ts
@@ -0,0 +1,21 @@
+import 'rxjs/add/operator/toPromise';
+
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import { AnonymousCaseReport } from '../shared/models/anonymouscasereport.model';
+
+@Injectable()
+export class AnonymousCaseReportService {
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
+    constructor(private http: Http) { }
+
+    getAnonymousCaseReports(): Promise<Array<AnonymousCaseReport>> {
+        return this.http
+            .get('/api/anonymouscasereports', { headers: this.headers })
+            .toPromise()
+            .then((result) => { return result.json(); })
+            .catch((error) => console.error(error));
+    }
+}
