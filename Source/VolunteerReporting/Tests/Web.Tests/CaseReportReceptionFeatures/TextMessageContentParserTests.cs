b3f1f0ec6070a26c734ee814a216afc1e68fd8b2
Roar Fredriksen <roarfred@gmail.com>
2017-10-21 19:03:31 +0200

    Updates as the parser was moved to the read project

--- Source/VolunteerReporting/Tests/Web.Tests/CaseReportReceptionFeatures/TextMessageContentParserTests.cs
+++ Source/VolunteerReporting/Tests/Web.Tests/CaseReportReceptionFeatures/TextMessageContentParserTests.cs
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using Read.TextMessageRecievedFeatures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
