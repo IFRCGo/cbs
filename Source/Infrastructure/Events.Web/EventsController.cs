f9abd4f189dd51ad9cb222ca05f03a24dae032d9
Einar Ingebrigtsen <einar@dolittle.com>
2017-10-22 17:21:25 +0200

    Hooking up EventSource + UncommittedEventStreamCoordinator

--- Source/Infrastructure/Events.Web/EventsController.cs
+++ Source/Infrastructure/Events.Web/EventsController.cs
@@ -1,19 +1,25 @@
 using System;
 using System.Linq;
 using doLittle.Events;
+using doLittle.Runtime.Events;
+using doLittle.Runtime.Events.Coordination;
+using doLittle.Runtime.Transactions;
 using doLittle.Types;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace Infrastructure.Events.Web
 {
+
     [Route("/api/events")]
     public class EventsController
     {
         readonly ITypeFinder _typeFinder;
+        readonly IUncommittedEventStreamCoordinator _uncommittedEventStreamCoordinator;
 
-        public EventsController(ITypeFinder typeFinder)
+        public EventsController(ITypeFinder typeFinder, IUncommittedEventStreamCoordinator uncommittedEventStreamCoordinator)
         {
+            _uncommittedEventStreamCoordinator = uncommittedEventStreamCoordinator;
             _typeFinder = typeFinder;
         }
 
@@ -21,12 +27,16 @@ namespace Infrastructure.Events.Web
         public void Post([FromBody]Event @event)
         {
             var eventType = _typeFinder.All.Where(type => type.Name == @event.Name).SingleOrDefault();
-            if( eventType == null ) throw new ArgumentException("Cannot resolve Event Type from Name - please check the name again");
+            if (eventType == null) throw new ArgumentException("Cannot resolve Event Type from Name - please check the name again");
 
             var serialized = JsonConvert.SerializeObject(@event.Content);
-            var typed = JsonConvert.DeserializeObject(serialized,eventType) as IEvent;
+            var eventInstance = JsonConvert.DeserializeObject(serialized, eventType) as IEvent;
 
-            throw new NotImplementedException();
+            var eventSource = new EventsControllerEventSource(@event.EventSourceId);
+            eventSource.Apply(eventInstance);
+            _uncommittedEventStreamCoordinator.Commit(
+                TransactionCorrelationId.New(),
+                eventSource.UncommittedEvents);
         }
     }
 }
