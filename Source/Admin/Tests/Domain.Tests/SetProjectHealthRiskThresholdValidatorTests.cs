9f90c407f5d9fec350f63fa8558a507135e60090
Roar Fredriksen <roarfred@gmail.com>
2017-11-04 11:29:47 +0100

    Added tests for valid thresholds

--- Source/Admin/Tests/Domain.Tests/SetProjectHealthRiskThresholdValidatorTests.cs
+++ Source/Admin/Tests/Domain.Tests/SetProjectHealthRiskThresholdValidatorTests.cs
@@ -55,5 +55,33 @@ namespace Domain.Tests
             var validationResult = validator.Validate(new SetProjectHealthRiskThreshold() { ProjectId = projectId, HealthRiskId = anotherHealthRiskId, Threshold = 2 });
             Assert.True(validationResult.Errors.Any(), "The command should NOT validate when health risk does not belongs to project");
         }
+
+        [Theory]
+        [InlineData(-15, false)]
+        [InlineData(-1, false)]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        [InlineData(15, true)]
+        public void NegativeThreshold_OnlyPositiveThresholdShouldValidate(int threshold, bool shouldValidate)
+        {
+            var projectId = Guid.NewGuid();
+            var healthRiskId = Guid.NewGuid();
+
+            var projects = A.Fake<IProjects>();
+            A.CallTo(() => projects.GetById(A<Guid>._)).Returns(
+                new Project()
+                {
+                    Id = projectId,
+                    HealthRisks = new ProjectHealthRisk[]
+                    {
+                        new ProjectHealthRisk() { HealthRiskId = healthRiskId, Threshold = 1 }
+                    }
+                }
+            );
+
+            var validator = new SetProjectHealthRiskThresholdValidator(projects);
+            var validationResult = validator.Validate(new SetProjectHealthRiskThreshold() { ProjectId = projectId, HealthRiskId = healthRiskId, Threshold = threshold });
+            Assert.Equal(shouldValidate, !validationResult.Errors.Any());
+        }
     }
 }
