a753ea4f8fabcc288e267aa105d32ebcf4acbb82
Roar Fredriksen <roarfred@gmail.com>
2017-11-04 12:30:13 +0100

    Created test project for Read and Web Fixes issues #276 and #277

---
+++ Source/Admin/Tests/Web.Tests/Test.cs
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Xunit;
+using FakeItEasy;
+
+namespace Web.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
