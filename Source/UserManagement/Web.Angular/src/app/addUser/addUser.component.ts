60cd164ff6043ee1d9ebbd8ef69a338596766223
Cathinka Walberg <cathinka.walberg@bekk.no>
2018-01-16 21:10:41 +0100

    Start replacing material with bootstrap and adding modal

---
+++ Source/UserManagement/Web.Angular/src/app/addUser/addUser.component.ts
@@ -0,0 +1,31 @@
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+@Component({
+    selector: 'cbs-addUser',
+    templateUrl: './addUser.component.html'
+  })
+export class AddUserComponent {
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template)
+  }
+}
+
+ 
+// @Component({
+//   selector: 'demo-modal-service-static',
+//   templateUrl: './service-template.html'
+// })
+// export class DemoModalServiceStaticComponent {
+//   modalRef: BsModalRef;
+//   constructor(private modalService: BsModalService) {}
+ 
+//   openModal(template: TemplateRef<any>) {
+//     this.modalRef = this.modalService.show(template);
+//   }
+// }
--- Source/UserManagement/Web.Angular/src/app/homeComponent/home.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'cbs-home',
-    templateUrl: './home.component.html'
-  })
-  export class HomeComponent {
-  }
-  
