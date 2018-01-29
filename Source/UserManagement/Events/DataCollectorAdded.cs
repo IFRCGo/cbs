e77f789ccee4b2d19e2622037dd40fdd18031174
Bjørn Fossan Rasmussen <bjorn@tradesolution.no>
2018-01-29 20:59:02 +0100

    #355: Added some notes on what to do later

--- Source/UserManagement/Events/DataCollectorAdded.cs
+++ Source/UserManagement/Events/DataCollectorAdded.cs
@@ -16,5 +16,13 @@ namespace Events
         public string GpsLocation { get; set; }
         public string MobilePhoneNumber { get; set; }
         public string Email { get; set; }
+
+        //TODO: Change to longtitude and latitude. Also remove mobile and email(optional) into sepperate events. Same with preffered language?
+        //TODO: Change long and lat to decimal in all places where it is used
+        // public Guid Id { get; set; }
+        // public string FirstName { get; set; }
+        // public string LastName { get; set; }
+        // public double LocationLongitude { get; set; }
+        // public double LocationLatitude { get; set; }
     }
 }
