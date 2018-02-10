7c53f22b4fbc6a1d35b4d79db9efd8b723467881
michael <michael@dolittle.com>
2018-02-10 10:51:09 +0100

    Add required headers to files
    Add Copyright Header to each C# file in UserManagement

--- Source/UserManagement/Domain/DataCollectors/AddPhoneNumberInputValidator.cs
+++ Source/UserManagement/Domain/DataCollectors/AddPhoneNumberInputValidator.cs
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 using doLittle.FluentValidation.Commands;
 using FluentValidation;
 using System;
