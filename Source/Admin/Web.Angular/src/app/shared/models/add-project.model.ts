07b61146e95625d4166de243350758c002a1fa5e
SneX <icegirl-13@hotmail.com>
2018-02-10 13:52:22 +0100

    add Surveillance context options

--- Source/Admin/Web.Angular/src/app/shared/models/add-project.model.ts
+++ Source/Admin/Web.Angular/src/app/shared/models/add-project.model.ts
@@ -2,6 +2,10 @@ export class AddProject {
     id: string;
     name: string;
     nationalSocietyId: string;
+<<<<<<< HEAD
     dataOwnerId: string;
+=======
+    ownerUserId: string;
+>>>>>>> add Surveillance context options
     surveillanceId: string;
 }
