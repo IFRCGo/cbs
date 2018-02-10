5fe539f36a6364df74183c9ba622bc79520f60be
woksin <sindrewilting@gmail.com>
2018-02-10 11:04:15 +0100

    Added events for deleting Key Messages and Reply Messages

---
+++ Source/VolunteerReporting/Events.External/ReplyMessageDeleted.cs
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using doLittle.Events;
+using System;
+
+namespace Events.External
+{
+    class ReplyMessageDeleted : IEvent
+    {
+        public Guid Id { get; set; }
+    }
+}
