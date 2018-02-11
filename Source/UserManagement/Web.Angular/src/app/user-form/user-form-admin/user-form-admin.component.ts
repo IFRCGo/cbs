d4a328b086d6f59723ea709f3338a2b1df1344a4
“Sindre <sindre.saegrov@retriever.no>
2018-02-11 13:35:32 +0100

    add staff-user service to admin form

--- Source/UserManagement/Web.Angular/src/app/user-form/user-form-admin/user-form-admin.component.ts
+++ Source/UserManagement/Web.Angular/src/app/user-form/user-form-admin/user-form-admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Admin } from '../../domain/admin';
+import { StaffUserService } from '../../services/staff-user.service';
+import { Router } from '@angular/router';
 
 export const ADMIN_PATH = 'admin';
 
@@ -11,15 +13,29 @@ export const ADMIN_PATH = 'admin';
 export class UserFormAdminComponent implements OnInit {
 
   user: Admin;
+  public errorMsg = 'Something went wrong, try again later';
+  public successMsg = 'User added';
+  public success = false;
+  public error = false;
 
-  constructor() {
+
+  constructor(private staffUserService: StaffUserService, private router: Router) {
     this.user = new Admin({});
    }
 
   ngOnInit() {
   }
 
-  onSubmit() {
-
+  submit() {
+    this.staffUserService.saveUser(this.user).subscribe(
+      data => {
+        this.success = true;
+        console.log(data);
+        setTimeout(() => this.router.navigateByUrl(''), 3000)
+      },
+      error => {
+        this.error = true;
+        console.error(error)
+      })
   }
 }
