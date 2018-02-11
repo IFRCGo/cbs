97d3c2d486e90f977060ee2807fa9b76c6df5dc4
David Lindblad <dalind@microsoft.com>
2018-02-11 16:00:32 +0100

    WIP

--- Source/UserManagement/Read/GreetingGenerators/GreetingHistoryEventProcessor.cs
+++ Source/UserManagement/Read/GreetingGenerators/GreetingHistoryEventProcessor.cs
@@ -1,32 +1,32 @@
-// using Concepts;
-// using doLittle.Domain;
-// using doLittle.Events.Processing;
-// using Domain.MessageGenerators;
-// using Events;
-// using Events.External;
-// using Read.DataCollectors;
+using Concepts;
+using doLittle.Domain;
+using doLittle.Events.Processing;
+using Domain.MessageGenerators;
+using Events;
+using Events.External;
+using Read.DataCollectors;
 
-// namespace Read.GreetingGenerators
-// {
-//     public class GreetingHistoryEventProcessor : ICanProcessEvents 
-//     {
-//         readonly IGreetingHistories _greetingHistories;
+namespace Read.GreetingGenerators
+{
+    public class GreetingHistoryEventProcessor : ICanProcessEvents 
+    {
+        readonly IGreetingHistories _greetingHistories;
 
-//         public GreetingHistoryEventProcessor(IGreetingHistories greetingHistories)
-//         {
-//             _greetingHistories = greetingHistories;
-//         }
+        public GreetingHistoryEventProcessor(IGreetingHistories greetingHistories)
+        {
+            _greetingHistories = greetingHistories;
+        }
 
-//         public void Process(PhoneNumberAddedToDataCollector @event)
-//         {
-//             var greetingHistory = _greetingHistories.GetByPhoneNumber(@event.PhoneNumber) ?? new GreetingHistory(@event.Id);
-//             greetingHistory.PhoneNumber = @event.PhoneNumber;
-//             _greetingHistories.Save(greetingHistory);
-//         }
+        public void Process(PhoneNumberAddedToDataCollector @event)
+        {
+            var greetingHistory = _greetingHistories.GetByPhoneNumber(@event.PhoneNumber) ?? new GreetingHistory(@event.Id);
+            greetingHistory.PhoneNumber = @event.PhoneNumber;
+            _greetingHistories.Save(greetingHistory);
+        }
 
-//         public void Process(PhoneNumberRemovedFromDataCollector @event)
-//         {
-//             _greetingHistories.RemovePhoneNumber(@event.PhoneNumber);
-//         }
-//     }
-// }
+        public void Process(PhoneNumberRemovedFromDataCollector @event)
+        {
+            _greetingHistories.RemovePhoneNumber(@event.PhoneNumber);
+        }
+    }
+}
