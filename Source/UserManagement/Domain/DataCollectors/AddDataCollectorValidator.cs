e81ccc578467b88df17652e4d17852fc083a66c4
Bjørn Rasmussen <Bjorn@tradesolution.no>
2018-02-11 10:59:42 +0100

    #437: Fixed some validation messages as well

--- Source/UserManagement/Domain/DataCollectors/AddDataCollectorValidator.cs
+++ Source/UserManagement/Domain/DataCollectors/AddDataCollectorValidator.cs
@@ -9,11 +9,11 @@ namespace Domain
         {
             RuleFor(_ => _.FullName)
                 .NotEmpty()
-                .WithMessage("First name is not correct - Has to be defined");
+                .WithMessage("Full name is not correct - Has to be defined");
 
             RuleFor(_ => _.DisplayName)
                 .NotEmpty()
-                .WithMessage("Last name is not correct - Has to be defined");
+                .WithMessage("Display name is not correct - Has to be defined");
 
             //TODO: rest of the rules
         }
