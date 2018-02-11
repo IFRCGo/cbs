e9fba44dfee34ca2424bbbdee5a12d24d9d6119d
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-11 13:45:10 +0100

    Started on automatically configure Kafka connectionstring based on env. var.

---
+++ Source/Infrastructure/Kafka/KafkaModule.cs
@@ -0,0 +1,19 @@
+using System;
+using Autofac;
+
+namespace Kafka
+{
+    public class KafkaModule : Autofac.Module
+    {
+        const string KAFKA_CONNECTIONSTRING = "KAFKA_CONNECTIONSTRING";
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            var environmentVariables = Environment.GetEnvironmentVariables();
+            var kafkaConnectionString = "52.178.92.69:9092";
+            if (environmentVariables.Contains(KAFKA_CONNECTIONSTRING))kafkaConnectionString = (string)environmentVariables[KAFKA_CONNECTIONSTRING];
+            
+            // _.For<TopicMessageSettings>().Use(() => new TopicMessageSettings { Server = kafkaConnectionString }).SetLifecycleTo(Lifecycles.Singleton);
+        }
+    }
+}
