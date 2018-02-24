2b1468ecb2827d947cd720255f5a49aece0e64c5
Einar Ingebrigtsen <einar@dolittle.com>
2018-02-24 08:43:27 +0100

    Removing configuration done in lower Kafka implementations

--- Source/Infrastructure/Kafka.BoundedContexts/BoundedContextModule.cs
+++ Source/Infrastructure/Kafka.BoundedContexts/BoundedContextModule.cs
@@ -16,8 +16,10 @@ namespace Kafka.BoundedContexts
             builder.RegisterType<BoundedContextListener>().As<IBoundedContextListener>().SingleInstance();
             
             var environmentVariables = Environment.GetEnvironmentVariables();
-            var kafkaConnectionString = "52.178.92.69:9092";
-            if (environmentVariables.Contains(KAFKA_CONNECTIONSTRING))kafkaConnectionString = (string)environmentVariables[KAFKA_CONNECTIONSTRING];
+            
+
+            
+            //if (environmentVariables.Contains(KAFKA_CONNECTIONSTRING))kafkaConnectionString = (string)environmentVariables[KAFKA_CONNECTIONSTRING];
             
             // _.For<TopicMessageSettings>().Use(() => new TopicMessageSettings { Server = kafkaConnectionString }).SetLifecycleTo(Lifecycles.Singleton);
         }
