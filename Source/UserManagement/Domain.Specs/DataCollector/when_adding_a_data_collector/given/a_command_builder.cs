f8fda0305b489b84e18ff2ad4ef5da5f6ef04f9d
woksin <sindrewilting@gmail.com>
2018-02-27 17:37:18 +0100

    Uncommented and updated references

---
+++ Source/UserManagement/Domain.Specs/DataCollector/when_adding_a_data_collector/given/a_command_builder.cs
@@ -0,0 +1,44 @@
+ using System;
+ using Concepts;
+ using System.Collections.Generic;
+ using Domain.DataCollector.Add;
+
+ namespace Domain.Specs.DataCollector.when_adding_a_data_collector.given
+ {
+     public class a_command_builder
+     {
+         public static AddDataCollector get_valid_command()
+         {
+             return  new AddDataCollector
+             {
+                 DataCollectorId = Guid.NewGuid(),
+                 FullName = "Data Collector",
+                 DisplayName = "Daty",
+                 YearOfBirth = 1980,
+                 Sex = Sex.Male,
+                 NationalSociety = Guid.NewGuid(),
+                 PreferredLanguage = Language.English,
+                 GpsLocation = new Location(123,123),
+                 PhoneNumbers = new List<string>{"123456789"},
+                 Email = "test@test.com"
+             };
+         }
+
+         public static AddDataCollector get_invalid_command(IEnumerable<Action<AddDataCollector>> invalidations)
+         {
+             var cmd = get_valid_command();
+             foreach(var invalidate in invalidations)
+             {
+                 invalidate(cmd);
+             }
+             return cmd;
+         }
+
+
+         public static AddDataCollector get_invalid_command(Action<AddDataCollector> invalidate)
+         {
+             return get_invalid_command(new Action<AddDataCollector>[]{ invalidate });
+         }
+     }
+ }
+
