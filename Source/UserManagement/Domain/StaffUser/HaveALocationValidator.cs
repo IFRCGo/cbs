6381f22f5fe3257212c5adee0fd141ea191bf581
michael <michael@dolittle.com>
2018-03-04 12:18:43 +0100

    Introduct ISupportPhoneNumbers and IRequirePhoneNumbers to differentiate been optional/required

--- Source/UserManagement/Domain/StaffUser/HaveALocationValidator.cs
+++ Source/UserManagement/Domain/StaffUser/HaveALocationValidator.cs
@@ -12,6 +12,5 @@ namespace Domain.StaffUser
                 .NotNull().WithMessage("Location must be provided")
                 .Must(l => l.IsValid()).WithMessage("Location is invalid.  Latitude must be in the range -90 to 90 and longitude in the range -180 to 180");
         }
-
     }
 }
---
+++ Source/UserManagement/Domain/StaffUser/IRequirePhoneNumbers.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Domain.StaffUser
+{
+    public interface IRequirePhoneNumbers
+    {
+         IEnumerable<string> PhoneNumbers { get; }
+    }
+}
