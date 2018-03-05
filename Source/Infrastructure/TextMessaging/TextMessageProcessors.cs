1df5bcf2fd854fd2084a5b2c19c5e4cb1c31eb14
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-05 21:16:57 +0100

    Adding exception handling and logging errors

--- Source/Infrastructure/TextMessaging/TextMessageProcessors.cs
+++ Source/Infrastructure/TextMessaging/TextMessageProcessors.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using doLittle.Collections;
 using doLittle.DependencyInversion;
+using doLittle.Logging;
 using doLittle.Runtime.Commands;
 using doLittle.Runtime.Transactions;
 using doLittle.Types;
@@ -12,27 +14,40 @@ namespace Infrastructure.TextMessaging
     {
         readonly IInstancesOf<ICanProcessTextMessage> _processors;
         readonly ICommandContextManager _commandContextManager;
+        readonly ILogger _logger;
 
         public TextMessageProcessors(
             IInstancesOf<ICanProcessTextMessage> processors,
-            ICommandContextManager commandContextManager)
+            ICommandContextManager commandContextManager,
+            ILogger logger)
         {
             _processors = processors;
             _commandContextManager = commandContextManager;
+            _logger = logger;
         }
 
-        public bool HasProcessors => _processors?.Any() ?? false;
+        public bool HasProcessors => _processors?.Any()?? false;
 
         public void Process(TextMessage message)
         {
             var commandContext = _commandContextManager.EstablishForCommand(
                 new CommandRequest(
-                    TransactionCorrelationId.New(), 
+                    TransactionCorrelationId.New(),
                     null,
                     new Dictionary<string, object>())
-                );
-            
-            _processors.ForEach(processor => processor.Process(message));
+            );
+
+            _processors.ForEach(processor =>
+            {
+                try
+                { 
+                    processor.Process(message);
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error(ex, "Problems processing message");
+                }
+            });
 
             commandContext.Commit();
         }
