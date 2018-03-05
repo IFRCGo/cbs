a1d5b70ea5b218b5cc3eb0103c28d76fb830616d
Einar Ingebrigtsen <einar@dolittle.com>
2018-03-05 20:04:53 +0100

    Establishing command context for text messages coming in

--- Source/Infrastructure/TextMessaging/TextMessageProcessors.cs
+++ Source/Infrastructure/TextMessaging/TextMessageProcessors.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Linq;
 using doLittle.Collections;
 using doLittle.DependencyInversion;
+using doLittle.Runtime.Commands;
+using doLittle.Runtime.Transactions;
 using doLittle.Types;
 
 namespace Infrastructure.TextMessaging
@@ -8,17 +11,30 @@ namespace Infrastructure.TextMessaging
     public class TextMessageProcessors : ITextMessageProcessors
     {
         readonly IInstancesOf<ICanProcessTextMessage> _processors;
+        readonly ICommandContextManager _commandContextManager;
 
-        public TextMessageProcessors(IInstancesOf<ICanProcessTextMessage> processors)
+        public TextMessageProcessors(
+            IInstancesOf<ICanProcessTextMessage> processors,
+            ICommandContextManager commandContextManager)
         {
             _processors = processors;
+            _commandContextManager = commandContextManager;
         }
 
         public bool HasProcessors => _processors?.Any() ?? false;
 
         public void Process(TextMessage message)
         {
+            var commandContext = _commandContextManager.EstablishForCommand(
+                new CommandRequest(
+                    TransactionCorrelationId.New(), 
+                    null,
+                    new Dictionary<string, object>())
+                );
+            
             _processors.ForEach(processor => processor.Process(message));
+
+            commandContext.Commit();
         }
     }
 }
