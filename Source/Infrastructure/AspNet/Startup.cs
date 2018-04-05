f12cdc233f0598b0f59def9e873c1945482b55c7
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-05 12:33:28 +0200

    Everything compiles for VolunteerReporting at least

--- Source/Infrastructure/AspNet/Startup.cs
+++ Source/Infrastructure/AspNet/Startup.cs
@@ -2,6 +2,8 @@
  *  Copyright (c) 2017-2018 The International Federation of Red Cross and Red Crescent Societies. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+using System.IO;
+using System.Threading.Tasks;
 using Autofac;
 using Dolittle.DependencyInversion.Autofac;
 using Infrastructure.AspNet.ConnectionStrings;
@@ -9,8 +11,10 @@ using Infrastructure.Kafka.BoundedContexts;
 using Infrastructure.TextMessaging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders.Physical;
 using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
 
@@ -80,6 +84,17 @@ namespace Infrastructure.AspNet
             TextMessageListener.Start(app.ApplicationServices);
 
             ConfigureCustom(app, env);
+
+            // Keep this last as this is the fallback when nothing else works - spit out the index file - SPA service replacement
+            // Microsoft SPA Services has an accidental coupling to MVC Controllers and its route builder, besides all it is trying to do is fallback
+            // when nothing else works - regardless of route builders or anything else
+            app.Run(async context =>
+            {
+                if( Path.HasExtension(context.Request.Path)) await Task.CompletedTask;
+                context.Request.Path = new PathString("/");
+                var path = $"{env.ContentRootPath}/wwwroot/index.html";
+                await context.Response.SendFileAsync(new PhysicalFileInfo(new FileInfo(path)));
+            });            
         }
 
 
