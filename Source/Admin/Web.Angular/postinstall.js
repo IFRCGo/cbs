94b2326c88f9b3632a31e73a6fb60fcaf7554cff
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-10 21:58:38 +0200

    Since we need elevated privileges for symlinks on Windows - copy

--- Source/Admin/Web.Angular/postinstall.js
+++ Source/Admin/Web.Angular/postinstall.js
@@ -1,8 +1,24 @@
-const fs = require('fs-extra');
+const fs = require('fs');
+const path = require('path');
+
+var copyRecursiveSync = function(src, dest) {
+  var exists = fs.existsSync(src);
+  var stats = exists && fs.statSync(src);
+  var isDirectory = exists && stats.isDirectory();
+  if (exists && isDirectory) {
+    fs.mkdirSync(dest);
+    fs.readdirSync(src).forEach(function(childItemName) {
+      copyRecursiveSync(path.join(src, childItemName),
+                        path.join(dest, childItemName));
+    });
+  } else {
+    fs.linkSync(src, dest);
+  }
+};
+
 fs.exists('./src/app/navigation', result => {
     if (!result) {
-        fs.ensureSymlink('../../Navigation/', './src/app/navigation', 'dir').then(() => {
-            console.log("Symbolic link setup done");
-        });
+        copyRecursiveSync('../../Navigation/', './src/app/navigation');
+        console.log("Navigation folder copied")
     }
 });
