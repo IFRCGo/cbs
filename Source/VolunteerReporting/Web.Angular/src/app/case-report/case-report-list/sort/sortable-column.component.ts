04da3f211456b0ed9ca78bb602af0d809a1e28c6
Jakob Høgenes <jakob.hogenes@sintef.no>
2018-04-22 00:35:35 +0200

    Refactor Case Report table and store sorting in URL
    Changed the sortable table used in case report listing. It's a bit more
    generic now, and could easily be made completely generic if we want to
    re-use a sortable table somewhere else in the frontends.
    
    Also, the sorting is now stored in the URL, so that it is kept after
    refreshing the page to get new data.

--- Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sortable-column.component.ts
+++ Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sortable-column.component.ts
@@ -1,48 +1,27 @@
-import {Component, OnInit, Input, HostListener} from '@angular/core';
-import {Subscription} from 'rxjs/Subscription';
+import { Component, Input } from '@angular/core';
 
-import {SortService} from './sort.service';
+import { Column, SortableColumn } from './columns';
 
 
 @Component({
-  selector: '[sortable-column]',
-  templateUrl: './sortable-column.component.html'
+    selector: '[column]',
+    templateUrl: './sortable-column.component.html'
 })
-export class SortableColumnComponent implements OnInit {
+export class SortableColumnComponent {
+    @Input('column')
+    column: Column;
 
-  constructor(private sortService: SortService) {
-  }
+    @Input('current-sorted')
+    current: SortableColumn;
 
-  @Input('sortable-column')
-  columnName: string;
+    @Input('sort-descending')
+    descending: boolean;
 
-  @Input('sort-direction')
-  sortDirection: string = '';
-
-  private columnSortedSubscription: Subscription;
-
-  @HostListener('click')
-  sort() {
-    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-
-    this.sortService.columnSorted({sortColumn: this.columnName, sortDirection: this.sortDirection});
-  }
-
-  ngOnInit() {
-    // subscribe to sort changes so we can react when other columns are sorted
-    this.columnSortedSubscription = this.sortService.columnSorted$.subscribe(event => {
-      // reset this column's sort direction to hide the sort icons
-      if (this.columnName != event.sortColumn) {
-        this.sortDirection = '';
-      }
-    });
-    // if a sorting is set already, emit the event
-    if (this.sortDirection !== '') {
-      this.sortService.columnSorted({sortColumn: this.columnName, sortDirection: this.sortDirection});
+    isSortedAscending(): boolean {
+        return this.column === this.current && !this.descending;
     }
-  }
 
-  ngOnDestroy() {
-    this.columnSortedSubscription.unsubscribe();
-  }
+    isSortedDescending(): boolean {
+        return this.column === this.current && this.descending;
+    }
 }
