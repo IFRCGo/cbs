62ba0ce136f68e3fd21d04b7800f10bdfe0e88c6
Einar Ingebrigtsen <einar@dolittle.com>
2018-04-22 20:31:11 +0200

    Using command directly in the frontend

--- Source/UserManagement/Web.Angular/src/app/user-form/user-form-data-collector/user-form-data-collector.component.ts
+++ Source/UserManagement/Web.Angular/src/app/user-form/user-form-data-collector/user-form-data-collector.component.ts
@@ -1,33 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
-import { DataCollector } from '../../domain/data-collector';
-import { DataCollectorService } from '../../services/data-collector.service';
+import { Router } from "@angular/router";
+import { RegisterDataCollector } from '../../domain/RegisterDataCollector';
+
+import { CommandCoordinator } from '../../services/CommandCoordinator';
+import { Command } from '../../services/Command';
+import { Guid } from '../../services/Guid';
 
 export const DATA_COLLECTOR_PATH = 'data-collector';
 
 @Component({
-  templateUrl: './user-form-data-collector.component.html',
-  styleUrls: ['./user-form-data-collector.component.scss']
+    templateUrl: './user-form-data-collector.component.html',
+    styleUrls: ['./user-form-data-collector.component.scss']
 })
 export class UserFormDataCollectorComponent {
 
-  user: DataCollector = new DataCollector({});
-  languageOptions = ['English', 'French'];
-  sexOptions = [{desc: 'Male', id: '1'}, {desc: 'Female', id: '2'}];
+    phoneNumberString: string;
+    command: RegisterDataCollector = new RegisterDataCollector();
+    languageOptions = ['English', 'French'];
+    sexOptions = [{ desc: 'Male', id: '1' }, { desc: 'Female', id: '2' }];
 
-  constructor(
-    private router: Router,
-    private dataCollectorService: DataCollectorService, 
-  ) {}
+    constructor(
+        private router: Router,
+        private commandCoordinator: CommandCoordinator
+    ) { }
 
-  submit() {
-    this.dataCollectorService.saveDataCollector(this.user)
-      .then(response => {
-        console.log(response); 
-      })
-      .catch(response => {
-        console.log(response);
-      });
-      this.router.navigate(['list']);
-  }
+    submit() {
+        this.command.dataCollectorId = Guid.create();
+        this.command.phoneNumbers = this.phoneNumberString.split(',');
+        this.commandCoordinator.handle(this.command)
+            .then(response => {
+                console.log(response); 
+                //this.router.navigate(['list']);
+            })
+            .catch(response => {
+                console.log(response);
+                //this.router.navigate(['list']);
+            });
+    }
 }
