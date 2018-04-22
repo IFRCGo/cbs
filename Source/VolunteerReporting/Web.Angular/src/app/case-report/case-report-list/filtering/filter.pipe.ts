IFRCGo/cbs

commit 04da3f211456b0ed9ca78bb602af0d809a1e28c6

Refactor Case Report table and store sorting in URL
Changed the sortable table used in case report listing. It's a bit more
generic now, and could easily be made completely generic if we want to
re-use a sortable table somewhere else in the frontends.

Also, the sorting is now stored in the URL, so that it is kept after
refreshing the page to get new data.

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/filtering/filter.pipe.ts
@@ -1,50 +1,42 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
 
+export class QuickFilter {
+    constructor(
+        public name: string,
+        public description: string,
+        public evaluate: (report:CaseReportForListing) => boolean
+    ) {}
+
+    static All: QuickFilter = new QuickFilter('all', 'All', report => {
+        return true;
+    });
+    static Success: QuickFilter = new QuickFilter('success', 'Success', report => {
+        return !!report.healthRiskId;
+    });
+    static Error: QuickFilter = new QuickFilter('error', 'Data error', report => {
+        return !report.healthRiskId;
+    });
+    static UnknownSender: QuickFilter = new QuickFilter('unknownSender', 'Unknown sender', report => {
+        return !report.dataCollectorId;
+    });
+
+    static Filters: Array<QuickFilter> = [
+        QuickFilter.All, QuickFilter.Success, QuickFilter.Error, QuickFilter.UnknownSender
+    ];
+
+    static fromName(name: string) : QuickFilter {
+        return QuickFilter.Filters.find(filter => filter.name == name) || QuickFilter.All;
+    }
+}
+
 @Pipe({
   name: 'filter',
   pure: false
 })
-
-//TODO: Add smarter filtering. Preferably a filter should be a json
 export class Filter implements PipeTransform {
-  transform(reports: Array<CaseReportForListing>, value: string) {
-    if (!reports || !value) {
-      return reports;
-    }
-    // I tried using Array.filter, but I did not get it to work for some reason...
-    // This should be optimized!!!
-    // TODO:
-    let list = [];
-    reports.forEach(report => {
-      switch (value) {
-        case 'all':
-          list.push(report);
-          break;
-        case 'success':
-          if (report.healthRiskId) {
-            list.push(report);
-          }
-          break;
-        case 'error':
-          if (!report.healthRiskId) {
-            list.push(report);
-          }
-          break;
-        case 'unknownSender':
-          if (!report.dataCollectorId) {
-            list.push(report);
-          }
-          break;
-
-        default:
-          list.push(report);
-          break;
-      }
-    });
-
-    return list;
+  transform(reports: Array<CaseReportForListing>, filter: QuickFilter) {
+    if (!reports) return [];
+    return reports.filter(filter.evaluate);
   }
-
-
 }

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/columns.ts
@@ -1,0 +1,74 @@
+import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
+
+export class Column {
+    constructor(
+      public name: string,
+      public description: string
+    ) {}
+}
+
+export class SortableColumn extends Column {
+    constructor(
+        public name: string,
+        public description: string,
+        // The compare function should return < 0 if a is to come before b when sorted in ascending order
+        public compare: (a: CaseReportForListing, b: CaseReportForListing) => number,
+        public predicate: (c: CaseReportForListing) => boolean
+    ){ super(name, description); }
+}
+
+const stringCompare = (a: string, b: string): number => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+}
+
+const hasHealthRisk = (c : CaseReportForListing): boolean => {
+  return c.healthRiskId !== null;
+}
+
+export const CaseReportColumns: Array<Column> = [
+    new SortableColumn(
+      'time', 'Time',
+      (a: CaseReportForListing, b: CaseReportForListing) => a.timestamp.valueOf() - b.timestamp.valueOf(),
+      (c: CaseReportForListing) => true
+    ),
+    new SortableColumn(
+      'status', 'Status',
+      (a: CaseReportForListing, b: CaseReportForListing) => Number(!!a.healthRiskId) - Number(!!b.healthRiskId),
+      (c: CaseReportForListing) => true
+    ),
+    new SortableColumn(
+      'dataCollector', 'Data Collector',
+      (a: CaseReportForListing, b: CaseReportForListing) => stringCompare(a.dataCollectorDisplayName, b.dataCollectorDisplayName),
+      hasHealthRisk
+    ),
+    new SortableColumn(
+      'healthRisk', 'Health Risk',
+      (a: CaseReportForListing, b: CaseReportForListing) => stringCompare(a.healthRisk, b.healthRisk),
+      hasHealthRisk
+    ),
+    new SortableColumn(
+      'malesAges0To4', 'Males < 5',
+      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfMalesUnder5 - b.numberOfMalesUnder5,
+      hasHealthRisk
+    ),
+    new SortableColumn(
+      'malesAgedOver4', 'Males ≥ 5',
+      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfMalesAged5AndOlder - b.numberOfMalesAged5AndOlder,
+      hasHealthRisk
+    ),
+    new SortableColumn(
+      'femalesAges0To4', 'Females < 5',
+      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfFemalesUnder5 - b.numberOfFemalesUnder5,
+      hasHealthRisk
+    ),
+    new SortableColumn(
+      'femalesAgesOver4', 'Females ≥ 5',
+      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfFemalesAged5AndOlder - b.numberOfFemalesAged5AndOlder,
+      hasHealthRisk
+    ),
+    new Column(
+      'latLong', 'Lat./Long.'
+    ),
+]

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sort.pipe.ts
@@ -1,0 +1,25 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
+
+import {SortableColumn} from './columns'
+
+@Pipe({
+    name: 'sort',
+    pure: false
+})
+export class Sort implements PipeTransform {
+    transform(reports: Array<CaseReportForListing>, column: SortableColumn, sortDescending: boolean) {
+        //console.log(reports, column, sortDescending);
+
+        if (!reports) return [];
+        return reports.sort((a: CaseReportForListing, b: CaseReportForListing) => {
+            const aIsSortable = column.predicate(a);
+            const bIsSortable = column.predicate(b);
+            // Show "non-sortable" elements last
+            if (aIsSortable && !bIsSortable) return -1;
+            if (!aIsSortable && bIsSortable) return 1;
+            // Otherwise, sort by comparison function (possibly reversed)
+            return column.compare(a, b) * (sortDescending ? -1 : 1);
+        })
+    }
+}

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/case-report-list.component.html
@@ -7,32 +7,26 @@
         <tr>
             <td>
                 Quick Filters:
-                <button (click)="basicFilter = 'all'">All</button>
-                <button (click)="basicFilter = 'success'">Success</button>
-                <button (click)="basicFilter = 'error'">Data error</button>
-                <button (click)="basicFilter = 'unknownSender'">Unknown sender</button>
+                <span *ngFor="let filter of allFilters">
+                  <button [ngStyle]="filterButtonStyle(filter)" (click)="clickFilter(filter)">{{filter.description}}</button>
+                </span>
             </td>
         </tr>
     </tbody>
 </table>
-<table class="table table-bordered table-striped" sortable-table (sorted)="onSorted($event)">
+<table class="table table-bordered table-striped">
     <thead>
         <tr>
-            <!-- To make a column sortable create the atribute sortable-column="sortColumn" and create an if-branch in ./sort/case-report.service.ts to
-            expand the sorting function for that given column -->
-            <th sortable-column="timeStamp" sort-direction="desc">Time</th>
-            <th sortable-column="status">Status</th>
-            <th sortable-column="dataCollector">Data&nbsp;Collector</th>
-            <th sortable-column="healthRisk">Health&nbsp;Risk</th>
-            <th sortable-column="malesAges0To4">Males &lt; 5</th>
-            <th sortable-column="malesAgedOver4">Males &ge; 5</th>
-            <th sortable-column="femalesAges0To4">Females &lt; 5</th>
-            <th sortable-column="femalesAgedOver4">Females &ge; 5</th>
-            <th>Lat&nbsp;/&nbsp;Long</th>
+            <th *ngFor="let column of allColumns"
+                  [column]="column"
+                  [current-sorted]="currentSortColumn"
+                  [sort-descending]="sortDescending"
+                  (click)="toggleSortColum(column)"
+            ></th>
         </tr>
     </thead>
     <tbody>
-        <tr *ngFor="let caseReport of listedReports | filter:basicFilter">
+        <tr *ngFor="let caseReport of listedReports | filter:currentFilter | sort:currentSortColumn:sortDescending">
             <td>{{caseReport.timestamp | date:'medium'}}</td>
             <td>
                 <span *ngIf="caseReport.healthRiskId" class="label label-success">Success</span>

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/case-report-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { AggregatedCaseReportService } from '../../core/aggregated-case-report.service';
 import { CaseReportForListing } from '../../shared/models/case-report-for-listing.model';
-
-import { ReportService, ReportSearchCriteria } from './sort/case-report.service';
-import { Filter } from './filtering/filter.pipe';
+import { Column, SortableColumn, CaseReportColumns } from './sort/columns';
+import { QuickFilter, Filter } from './filtering/filter.pipe';
 import { CaseReportExporter } from './exporter/case-report-exporter.service';
 import { Report } from '../../shared/models/report.model';
 
@@ -12,7 +13,6 @@ import { Report } from '../../shared/models/report.model';
     templateUrl: './case-report-list.component.html',
     styleUrls: ['./case-report-list.component.scss']
 })
-
 /**
  * maxReports: number
  *      The number of reports that can be shown on a page
@@ -29,59 +29,61 @@ import { Report } from '../../shared/models/report.model';
 export class CaseReportListComponent implements OnInit {
 
     listedReports: Array<CaseReportForListing>;
-    filterField: string;
-    filterValue: any;
-    lastCriteria: ReportSearchCriteria;
 
-    basicFilter: string = 'all';
+    allFilters: Array<QuickFilter> = QuickFilter.Filters;
+    currentFilter: QuickFilter = QuickFilter.All;
 
-    fields: Array<string> = [
-        'id', 'status', 'dataCollectorId', 'dataCollectorDisplayName',
-        'healthRiskId', 'healthRisk', 'message',
-        'numberOfFemalesOver5', 'numberOfFemalesUnder5',
-        'numberOfMalesOver5', 'numberOfMalesUnder5',
-        'timestamp', 'location', 'origin', 'parsingErrorMessage'
-    ];
+    allColumns: Array<Column> = CaseReportColumns;
+    sortDescending: boolean = true;
+    currentSortColumn: SortableColumn = CaseReportColumns[0] as SortableColumn; // Timestamp
 
-    constructor(private caseReportService: AggregatedCaseReportService,
-        private service: ReportService,
-        private caseReportExporter: CaseReportExporter) {
+    constructor(
+        private caseReportService: AggregatedCaseReportService,
+        private caseReportExporter: CaseReportExporter,
+        private route: ActivatedRoute,
+        private router: Router
+    ) { }
+
+    updateNavigation(filter: QuickFilter, column: SortableColumn, sortDescending: boolean) {
+      this.router.navigate(['../'+filter.name], { relativeTo: this.route, queryParams: {
+        sortBy: column.name,
+        order: sortDescending ? 'desc' : 'asc'
+      }});
     }
 
-    /**
-     * Calls a getReports method in a class that handles sorting on the data passed.
-     * It may, or may not, be done by sending both an array that holds all the reports referenced to by
-     * the Report interface and an array of raw CaseReport objects.
-     * Ideally it would be nice to just pass an array of the raw data
-     * @param criteria The sorting criteria passed to the sorter service.
-     */
-    getReports(criteria: ReportSearchCriteria) {
-        this.lastCriteria = criteria;
-        if (this.listedReports !== undefined) {
-          this.listedReports = this.service.getReports(this.listedReports, criteria);
+    filterButtonStyle(filter: QuickFilter) {
+        return {
+            'font-weight': this.currentFilter === filter ? 'bold' : 'normal'
+        };
+    }
+    clickFilter(filter: QuickFilter) {
+        this.updateNavigation(filter, this.currentSortColumn, this.sortDescending);
+    }
+
+    toggleSortColum(column: Column) {
+        if (column instanceof SortableColumn) {
+            if (column !== this.currentSortColumn) {
+                this.updateNavigation(this.currentFilter, column, true);
+            } else {
+                this.updateNavigation(this.currentFilter, this.currentSortColumn, !this.sortDescending);
+            }
         }
     }
 
-    onSorted($event) {
-        this.getReports($event);
-    }
-
     ngOnInit() {
-        this.getAllReports();
-    }
+        this.caseReportService.getReports().then(result => {
+            this.listedReports = result || [];
+        }).catch(error => console.error(error));
 
-    getAllReports() {
-        this.caseReportService.getReports()
-            .then(result => {
-                  this.listedReports = result || [];
-                  if (this.lastCriteria) {
-                    this.getReports(this.lastCriteria);
-                  }
-            })
-            .catch(error => console.error(error));
-    }
+        this.route.params.subscribe(params => {
+            this.currentFilter = QuickFilter.fromName(params.filter);
+        });
 
-    onClick(name: string) {
-        this.basicFilter = name;
+        this.route.queryParams.subscribe(query => {
+            this.sortDescending = query.order != 'asc';
+            this.currentSortColumn = this.allColumns.find(column => {
+                return column instanceof SortableColumn && column.name == query.sortBy;
+            }) as SortableColumn || this.currentSortColumn;
+        });
     }
 }

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/case-report.service.ts
@@ -1,105 +1,0 @@
-import {Injectable} from '@angular/core';
-
-import {Report} from '../../../shared/models/report.model';
-import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
-import {CaseReportStatus} from '../../../shared/models/case-report-status.model';
-
-@Injectable()
-export class ReportService {
-
-  static hasDC(report: CaseReportForListing) {
-    return report.dataCollectorId != null;
-  }
-
-  static hasHealthRisk(report: CaseReportForListing) {
-    return report.healthRiskId != null;
-  }
-
-  static compareStandard(a: CaseReportForListing, b: CaseReportForListing, field: string,
-                         predicate: (report: CaseReportForListing) => boolean, criteria: ReportSearchCriteria) {
-    const res = (!predicate(a) && !predicate(b)) ? 0
-      : (!predicate(a) && predicate(b)) ? -1
-        : (predicate(a) && !predicate(b)) ? 1
-          : (a[field] < b[field]) ? 1
-            : (a[field] > b[field]) ? -1 : 0;
-
-    return (criteria.sortDirection === 'desc') ? res : res * -1;
-  }
-
-  getReports(reports: Array<CaseReportForListing>, criteria: ReportSearchCriteria) {
-
-    if (criteria.sortColumn === 'timeStamp') {
-      return reports.sort(function (a, b) {
-        return (criteria.sortDirection === 'desc') ?
-          a.timestamp < b.timestamp ? 1 : a.timestamp > b.timestamp ? -1 : 0
-          : a.timestamp > b.timestamp ? 1 : a.timestamp < b.timestamp ? -1 : 0;
-      });
-    }
-
-    // Sort by status
-    if (criteria.sortColumn === 'status') {
-      return reports.sort(function (a, b) {
-        const result =
-          ((a.healthRiskId && b.healthRiskId) || (!a.healthRiskId && !b.healthRiskId)) ?
-            0 : a.healthRiskId ? -1 : 1;
-
-        return (criteria.sortDirection === 'desc') ? result : result * -1;
-      });
-    }
-
-    // Sort by datacollector displayname
-    if (criteria.sortColumn === 'dataCollector') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'dataCollectorDisplayName',
-          ReportService.hasDC, criteria);
-      });
-    }
-
-    // Sort by HealthRisk
-    if (criteria.sortColumn === 'healthRisk') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'healthRisk',
-          ReportService.hasHealthRisk, criteria);
-      });
-    }
-
-    // Sort by femalesAgedOver4
-    if (criteria.sortColumn === 'femalesAgedOver4') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'numberOfFemalesAged5AndOlder',
-          ReportService.hasHealthRisk, criteria);
-      });
-    }
-
-    // Sort by femalesAges0To4
-    if (criteria.sortColumn === 'femalesAges0To4') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'numberOfFemalesUnder5',
-          ReportService.hasHealthRisk, criteria);
-      });
-    }
-
-    // Sort by malesOAgedOver4
-    if (criteria.sortColumn === 'malesAgedOver4') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'numberOfMalesAged5AndOlder',
-          ReportService.hasHealthRisk, criteria);
-      });
-    }
-
-    // Sort by malesAges0To4
-    if (criteria.sortColumn === 'malesAges0To4') {
-      return reports.sort(function (a, b) {
-        return ReportService.compareStandard(a, b, 'numberOfMalesUnder5',
-          ReportService.hasHealthRisk, criteria);
-      });
-    }
-
-    return reports;
-  }
-}
-
-export class ReportSearchCriteria {
-  sortColumn: string;
-  sortDirection: string;
-}

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sort.service.ts
@@ -1,23 +1,0 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs/Subject';
-
-@Injectable()
-export class SortService {
-
-  constructor() {
-  }
-
-  private columnSortedSource = new Subject<ColumnSortedEvent>();
-
-  columnSorted$ = this.columnSortedSource.asObservable();
-
-  columnSorted(event: ColumnSortedEvent) {
-    this.columnSortedSource.next(event);
-  }
-
-}
-
-export interface ColumnSortedEvent {
-  sortColumn: string;
-  sortDirection: string;
-}

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sortable-column.component.html
@@ -1,3 +1,3 @@
-<i class="fa fa-chevron-up" *ngIf="sortDirection === 'asc'" ></i>
-<i class="fa fa-chevron-down" *ngIf="sortDirection === 'desc'"></i>
-<ng-content></ng-content>
+<i class="fa fa-chevron-up" *ngIf="isSortedAscending()" ></i>
+<i class="fa fa-chevron-down" *ngIf="isSortedDescending()"></i>
+{{column.description}}

commit 2ca2c52be6866fba2d11bf208ef805eef3c4a386

Bugfix. Linter was failing after 04da3f2.
(I really need to remember to run the linter before I create a PR).

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/filtering/filter.pipe.ts
@@ -36,7 +36,7 @@ export class QuickFilter {
 })
 export class Filter implements PipeTransform {
   transform(reports: Array<CaseReportForListing>, filter: QuickFilter) {
-    if (!reports) return [];
+    if (!reports) { return []; }
     return reports.filter(filter.evaluate);
   }
 }

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/columns.ts
@@ -14,61 +14,71 @@ export class SortableColumn extends Column {
         // The compare function should return < 0 if a is to come before b when sorted in ascending order
         public compare: (a: CaseReportForListing, b: CaseReportForListing) => number,
         public predicate: (c: CaseReportForListing) => boolean
-    ){ super(name, description); }
+    ) { super(name, description); }
 }
 
 const stringCompare = (a: string, b: string): number => {
-    if (a < b) return -1;
-    if (a > b) return 1;
+    if (a < b) { return -1; }
+    if (a > b) { return 1; }
     return 0;
-}
+};
 
 const hasHealthRisk = (c : CaseReportForListing): boolean => {
   return c.healthRiskId !== null;
-}
+};
 
 export const CaseReportColumns: Array<Column> = [
     new SortableColumn(
-      'time', 'Time',
-      (a: CaseReportForListing, b: CaseReportForListing) => a.timestamp.valueOf() - b.timestamp.valueOf(),
-      (c: CaseReportForListing) => true
+        'time', 'Time',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            a.timestamp.valueOf() - b.timestamp.valueOf(),
+        (c: CaseReportForListing) =>
+            true
     ),
     new SortableColumn(
-      'status', 'Status',
-      (a: CaseReportForListing, b: CaseReportForListing) => Number(!!a.healthRiskId) - Number(!!b.healthRiskId),
-      (c: CaseReportForListing) => true
+        'status', 'Status',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            Number(!!a.healthRiskId) - Number(!!b.healthRiskId),
+        (c: CaseReportForListing) =>
+            true
     ),
     new SortableColumn(
-      'dataCollector', 'Data Collector',
-      (a: CaseReportForListing, b: CaseReportForListing) => stringCompare(a.dataCollectorDisplayName, b.dataCollectorDisplayName),
-      hasHealthRisk
+        'dataCollector', 'Data Collector',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            stringCompare(a.dataCollectorDisplayName, b.dataCollectorDisplayName),
+        hasHealthRisk
     ),
     new SortableColumn(
-      'healthRisk', 'Health Risk',
-      (a: CaseReportForListing, b: CaseReportForListing) => stringCompare(a.healthRisk, b.healthRisk),
-      hasHealthRisk
+        'healthRisk', 'Health Risk',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            stringCompare(a.healthRisk, b.healthRisk),
+        hasHealthRisk
     ),
     new SortableColumn(
-      'malesAges0To4', 'Males < 5',
-      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfMalesUnder5 - b.numberOfMalesUnder5,
-      hasHealthRisk
+        'malesAges0To4', 'Males < 5',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            a.numberOfMalesUnder5 - b.numberOfMalesUnder5,
+        hasHealthRisk
     ),
     new SortableColumn(
-      'malesAgedOver4', 'Males ≥ 5',
-      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfMalesAged5AndOlder - b.numberOfMalesAged5AndOlder,
-      hasHealthRisk
+        'malesAgedOver4', 'Males ≥ 5',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            a.numberOfMalesAged5AndOlder - b.numberOfMalesAged5AndOlder,
+        hasHealthRisk
     ),
     new SortableColumn(
-      'femalesAges0To4', 'Females < 5',
-      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfFemalesUnder5 - b.numberOfFemalesUnder5,
-      hasHealthRisk
+        'femalesAges0To4', 'Females < 5',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            a.numberOfFemalesUnder5 - b.numberOfFemalesUnder5,
+        hasHealthRisk
     ),
     new SortableColumn(
-      'femalesAgesOver4', 'Females ≥ 5',
-      (a: CaseReportForListing, b: CaseReportForListing) => a.numberOfFemalesAged5AndOlder - b.numberOfFemalesAged5AndOlder,
-      hasHealthRisk
+        'femalesAgesOver4', 'Females ≥ 5',
+        (a: CaseReportForListing, b: CaseReportForListing) =>
+            a.numberOfFemalesAged5AndOlder - b.numberOfFemalesAged5AndOlder,
+        hasHealthRisk
     ),
     new Column(
       'latLong', 'Lat./Long.'
     ),
-]
+];

Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/sort/sort.pipe.ts
@@ -1,7 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
 
-import {SortableColumn} from './columns'
+import {SortableColumn} from './columns';
 
 @Pipe({
     name: 'sort',
@@ -9,17 +9,15 @@ import {SortableColumn} from './columns'
 })
 export class Sort implements PipeTransform {
     transform(reports: Array<CaseReportForListing>, column: SortableColumn, sortDescending: boolean) {
-        //console.log(reports, column, sortDescending);
-
-        if (!reports) return [];
+        if (!reports) { return []; }
         return reports.sort((a: CaseReportForListing, b: CaseReportForListing) => {
             const aIsSortable = column.predicate(a);
             const bIsSortable = column.predicate(b);
             // Show "non-sortable" elements last
-            if (aIsSortable && !bIsSortable) return -1;
-            if (!aIsSortable && bIsSortable) return 1;
+            if (aIsSortable && !bIsSortable) { return -1; }
+            if (!aIsSortable && bIsSortable) { return 1; }
             // Otherwise, sort by comparison function (possibly reversed)
             return column.compare(a, b) * (sortDescending ? -1 : 1);
-        })
+        });
     }
 }