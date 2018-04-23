f57933c748242fb9221b5c0a2805025c8d7316cf
woksin <sindrewilting@gmail.com>
2018-04-23 16:15:03 +0200

    Changed routing in angular solution. Moved to exporter service.

--- Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/exporter/case-report-exporter.service.ts
+++ Source/VolunteerReporting/Web.Angular/src/app/case-report/case-report-list/exporter/case-report-exporter.service.ts
@@ -1,10 +1,17 @@
 import {Injectable} from '@angular/core';
 import {CaseReportForListing} from '../../../shared/models/case-report-for-listing.model';
 
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/observable/forkJoin';
+
+import {Headers, Http} from '@angular/http';
+
+import {environment} from '../../../../environments/environment';
 
 @Injectable()
 export class CaseReportExporter {
-  constructor() {
+  private headers = new Headers({'Content-Type': 'application/json'});
+  constructor(private http: Http) {
   }
 
   exportToCsv(listedReports: Array<CaseReportForListing>, fields: Array<string>) {
@@ -15,5 +22,18 @@ export class CaseReportExporter {
     // We really should have someone that has experience with this frontend here.
     // --Woksin
 
+    // TODO: From woksin: I think that this shoul be a backend thing. The way I'm seeing it we only want
+    // to do a request call with the filters we want to use, and what we are sorting on perhaps, and let the
+    // backend do the job. Sending an Array of CaseReportForListing would be pretty slow.
+
+  }
+  exportToPdf(/* filters, orderby */): Promise<void | any> {
+    //TODO: Export to pdf should work on the backend now, we just need to make a button for exporting
+    return this.http.get(`${environment.api}/api/casereports/export/pdf`, {headers: this.headers})
+      .toPromise()
+      .then(res => {
+          return res;
+      })
+      .catch(error => console.error(error));
   }
 }
--- Source/VolunteerReporting/Web.Angular/src/app/core/aggregated-case-report.service.ts
+++ Source/VolunteerReporting/Web.Angular/src/app/core/aggregated-case-report.service.ts
@@ -55,14 +55,4 @@ export class AggregatedCaseReportService {
       })
       .catch(error => console.error(error));
   }
-
-  exportToPdf(): Promise<void | any> {
-    //TODO: Export to pdf should work on the backend now, we just need to make a button for exporting
-    return this.http.get(`${environment.api}/api/casereports/pdf`, {headers: this.headers})
-      .toPromise()
-      .then(res => {
-          return res;
-      })
-      .catch(error => console.error(error));
-  }
 }
