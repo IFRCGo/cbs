64ff31f3c41d9ec972fb9c06c3dd29603865aa8c
John Martin Dahl <kaalsaas@hotmail.com>
2018-09-29 08:24:26 +0200

    Added licence

--- Source/Infrastructure/Rules/IRuleImplementationFor.cs
+++ Source/Infrastructure/Rules/IRuleImplementationFor.cs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2018 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 using System;
 
 namespace Infrastructure.Rules
