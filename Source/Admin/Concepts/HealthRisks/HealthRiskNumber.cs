50ed6115651c6911f595e6f8e0fcb679d518f2f4
Einar Ingebrigtsen <einar@dolittle.com>
2018-09-30 19:51:22 +0200

    Introducing HealthRiskNumber

--- Source/Admin/Concepts/HealthRisks/HealthRiskNumber.cs
+++ Source/Admin/Concepts/HealthRisks/HealthRiskNumber.cs
@@ -5,5 +5,5 @@ namespace Concepts.HealthRisks
     public class HealthRiskNumber : ConceptAs<int>
     {
         public static implicit operator HealthRiskNumber(int number) => new HealthRiskNumber { Value = number };
-    }    
+    }   
 }
