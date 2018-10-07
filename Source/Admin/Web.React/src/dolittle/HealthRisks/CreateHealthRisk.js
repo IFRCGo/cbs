192928e887dba82a9db67c363c4d75e2f1fc0f56
Nyranith <kaalsaas@hotmail.com>
2018-10-07 15:26:58 +0200

    Autogenerated files from the dolittle framework with the dotnet project and created a single import to simplify the usage.

---
+++ Source/Admin/Web.React/src/dolittle/HealthRisks/CreateHealthRisk.js
@@ -0,0 +1,21 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class CreateHealthRisk extends Command
+{
+    constructor() {
+        super();
+        this.type = 'f519fe4c-e353-4b86-8289-2a1cef497350';
+
+        this.id = '00000000-0000-0000-0000-000000000000';
+        this.name = '';
+        this.readableId = 0;
+        this.caseDefinition = '';
+        this.note = '';
+        this.communityCase = '';
+        this.keyMessage = '';
+    }
+}
---
+++ Source/Admin/Web.React/src/dolittle/HealthRisks/ModifyHealthRisk.js
@@ -0,0 +1,21 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Command Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Command } from  '@dolittle/commands';
+
+export class ModifyHealthRisk extends Command
+{
+    constructor() {
+        super();
+        this.type = '016afd4f-e190-48a4-8e04-3d13b95c0414';
+
+        this.id = '00000000-0000-0000-0000-000000000000';
+        this.name = '';
+        this.readableId = 0;
+        this.caseDefinition = '';
+        this.note = '';
+        this.communityCase = '';
+        this.keyMessage = '';
+    }
+}
