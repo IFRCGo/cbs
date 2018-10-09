bb1668fc8f258cc8b349660a1c4646ef5f84d8ff
Nyranith <kaalsaas@hotmail.com>
2018-10-09 23:01:32 +0200

    Working on the css for the cbs navigation bar.

--- Source/Admin/Web.React/src/App.js
+++ Source/Admin/Web.React/src/App.js
@@ -4,27 +4,13 @@ import { QueryCoordinator } from '@dolittle/queries';
 import { CommandCoordinator } from '@dolittle/commands';
 import './App.css';
 import { AddDataVerifier } from './dolittle.imports'; 
-import { Fancy } from 'cbs-navigation';
+import { Navigation } from 'cbs-navigation'; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. 
-            <Fancy></Fancy>
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation></Navigation>
       </div>
     );
   }
--- Source/cbs.Navigation/src/navigation/Navigation.js
+++ Source/cbs.Navigation/src/navigation/Navigation.js
@@ -3,9 +3,42 @@ import './Navigation.css';
 
 
 export class Navigation extends React.Component{
+        
+    constructor(props) {
+        super(props);
+
+        this.name = "TEST"; 
+    }
+
     render() {
         return (
-          <div>This is so Fancy!</div>
+            <header class="navigation-header">
+                <figure class="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="700" height="400" viewBox="0 0 175 100">
+                        <rect width="175" height="100" fill="#fff" />
+                        <path d="M20,50h66m-33,-33v66" fill="none" stroke="#c00" stroke-width="20" />
+                        <circle cx="132" cy="50" r="34" fill="#c00" />
+                        <circle cx="142" cy="50" r="28" fill="#fff" />
+                        <path d="M7,7H168V93H7z" fill="none" stroke="#c00" stroke-width="3" />
+                    </svg>
+                </figure>
+                <nav>
+                    <a href="/">Project administration</a>
+                    <a href="/users">User management</a>
+                    <a href="/reporting">Volunteer reporting</a>
+                    <a href="/alerts">Alerts</a>
+                    <a href="/analytics">Analytics</a>
+                </nav>
+
+                <div class="login-status">
+                    <div class="logged-in">
+                        <p>
+                            Logged in as:
+                        </p>
+                        <p>{this.name}</p>
+                    </div>
+                </div>
+            </header>
         );
     }
 }
