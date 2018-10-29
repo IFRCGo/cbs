b69c34883c867a4ce3c4bdc1af4b22f8fea6d812
John Martin <kaalsaas@hotmail.com>
2018-10-29 07:56:02 +0100

    Fixing up in the url, mabye just have one instance of the QueryCoordinator.

--- Source/Admin/Web.React/src/app/utils/QueryConfig.js
+++ Source/Admin/Web.React/src/app/utils/QueryConfig.js
@@ -1,10 +1,14 @@
 
 import { QueryCoordinator } from '@dolittle/queries';
+import { CommandCoordinator } from '@dolittle/commands';
 import config from '../../config'; 
 
 console.warn(config.apiGateway.URL);
 
+QueryCoordinator.apiBaseUrl = config.apiGateway.URL;
+CommandCoordinator.apiBaseUrl = config.apiGateway.URL;
+
 var queryCoordinator = new QueryCoordinator(config.apiGateway.URL); 
-queryCoordinator.apiBaseUrl = config.apiGateway.URL;
+
 
 export default queryCoordinator;
