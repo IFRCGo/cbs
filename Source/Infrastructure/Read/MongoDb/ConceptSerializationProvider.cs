4b02d375464c4854aa53c1d7ec421ac37d399ae6
John Martin Dahl <kaalsaas@hotmail.com>
2018-10-31 23:19:25 +0100

    Testing out some logic with the concept serializers.

--- Source/Infrastructure/Read/MongoDb/ConceptSerializationProvider.cs
+++ Source/Infrastructure/Read/MongoDb/ConceptSerializationProvider.cs
@@ -9,12 +9,46 @@ namespace Infrastructure.Read.MongoDb
     /// </summary>
     public class ConceptSerializationProvider : IBsonSerializationProvider
     {
+
         /// <inheritdoc/>
         public IBsonSerializer GetSerializer(Type type)
         {
             if (type.IsConcept())
-                return new ConceptSerializer(type);
-
+            {
+                if(type.IsSubclassOf(typeof(ConceptAs<Guid>)))
+                {
+                    return new ConceptSerializer<ConceptAs<Guid>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<double>)))
+                {
+                    return new ConceptSerializer<ConceptAs<double>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<float>)))
+                {
+                    return new ConceptSerializer<ConceptAs<float>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<Int32>)))
+                {
+                    return new ConceptSerializer<ConceptAs<Int32>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<Int64>)))
+                {
+                    return new ConceptSerializer<ConceptAs<Int64>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<bool>)))
+                {
+                    return new ConceptSerializer<ConceptAs<bool>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<string>)))
+                {
+                    return new ConceptSerializer<ConceptAs<string>>();
+                }
+                else if (type.IsSubclassOf(typeof(ConceptAs<decimal>)))
+                {
+                    return new ConceptSerializer<ConceptAs<decimal>>();
+                }
+            }
+                
             return null;
         }
     }
--- Source/Infrastructure/Read/MongoDb/ConceptSerializer.cs
+++ Source/Infrastructure/Read/MongoDb/ConceptSerializer.cs
@@ -7,10 +7,177 @@ using MongoDB.Bson.Serialization;
 
 namespace Infrastructure.Read.MongoDb
 {
-     /// <summary>
+
+    /*public class ConceptSerializer
+    {
+        public static IBsonSerializer<T> CreateInstance<T>(T instance)
+        {
+            return new ConceptSerializer<T>(instance);
+        }
+    }*/
+
+    public class ConceptSerializer<T> : IBsonSerializer<T>
+    {
+        public Type ValueType { get; }
+
+        public ConceptSerializer()
+        {
+            if (!typeof(T).IsConcept())
+                throw new Exception("Not a concept");
+
+            ValueType = typeof(T); 
+        }
+
+        public ConceptSerializer(T value)
+        {
+            ValueType = value.GetType(); 
+        }
+
+        public T Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var bsonReader = context.Reader;
+
+            var actualType = args.NominalType;
+
+            object value = null;
+
+            BsonType bsonType = bsonReader.GetCurrentBsonType();
+
+            var valueType = actualType.GetConceptValueType();
+            if (bsonType == BsonType.Document) // It should be a Concept object
+            {
+                bsonReader.ReadStartDocument();
+                var keyName = bsonReader.ReadName(Utf8NameDecoder.Instance);
+                if (keyName == "Value" || keyName == "value")
+                {
+                    value = GetDeserializedValue(valueType, ref bsonReader);
+                    bsonReader.ReadEndDocument();
+                }
+                else
+                {
+                    //Throw exception
+                }
+
+            }
+            else
+            {
+                value = GetDeserializedValue(valueType, ref bsonReader);
+            }
+
+            var concept = ConceptFactory.CreateConceptInstance(typeof(T), value);
+
+            var ret = concept != null ? (T)concept : default(T);
+
+            return ret;
+        }
+
+        object IBsonSerializer.Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            return this.Deserialize(context, args); 
+        }
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, T value)
+        {
+            var underlyingValue = value?.GetType().GetTypeInfo().GetProperty("Value").GetValue(value, null);
+            var nominalType = args.NominalType;
+            var underlyingValueType = nominalType.GetConceptValueType();
+
+            var bsonWriter = context.Writer;
+
+            if (underlyingValueType == typeof(Guid))
+            {
+                var guid = (Guid)(underlyingValue ?? default(Guid));
+                var guidAsBytes = guid.ToByteArray();
+                bsonWriter.WriteBinaryData(new BsonBinaryData(guidAsBytes, BsonBinarySubType.UuidLegacy, GuidRepresentation.CSharpLegacy));
+            }
+            else if (underlyingValueType == typeof(double))
+                bsonWriter.WriteDouble((double)(underlyingValue ?? default(double)));
+            else if (underlyingValueType == typeof(float))
+                bsonWriter.WriteDouble((double)(underlyingValue ?? default(double)));
+            else if (underlyingValueType == typeof(Int32))
+                bsonWriter.WriteInt32((Int32)(underlyingValue ?? default(Int32)));
+            else if (underlyingValueType == typeof(Int64))
+                bsonWriter.WriteInt64((Int64)(underlyingValue ?? default(Int64)));
+            else if (underlyingValueType == typeof(bool))
+                bsonWriter.WriteBoolean((bool)(underlyingValue ?? default(bool)));
+            else if (underlyingValueType == typeof(string))
+                bsonWriter.WriteString((string)(underlyingValue ?? string.Empty));
+            else if (underlyingValueType == typeof(decimal))
+                bsonWriter.WriteString(underlyingValue?.ToString() ?? default(decimal).ToString());
+        }
+
+        public void Serialize(BsonSerializationContext context, BsonSerializationArgs args, object value)
+        {
+            var underlyingValue = value?.GetType().GetTypeInfo().GetProperty("Value").GetValue(value, null);
+            var nominalType = args.NominalType;
+            var underlyingValueType = nominalType.GetConceptValueType();
+
+            var bsonWriter = context.Writer;
+
+            if (underlyingValueType == typeof(Guid))
+            {
+                var guid = (Guid)(underlyingValue ?? default(Guid));
+                var guidAsBytes = guid.ToByteArray();
+                bsonWriter.WriteBinaryData(new BsonBinaryData(guidAsBytes, BsonBinarySubType.UuidLegacy, GuidRepresentation.CSharpLegacy));
+            }
+            else if (underlyingValueType == typeof(double))
+                bsonWriter.WriteDouble((double)(underlyingValue ?? default(double)));
+            else if (underlyingValueType == typeof(float))
+                bsonWriter.WriteDouble((double)(underlyingValue ?? default(double)));
+            else if (underlyingValueType == typeof(Int32))
+                bsonWriter.WriteInt32((Int32)(underlyingValue ?? default(Int32)));
+            else if (underlyingValueType == typeof(Int64))
+                bsonWriter.WriteInt64((Int64)(underlyingValue ?? default(Int64)));
+            else if (underlyingValueType == typeof(bool))
+                bsonWriter.WriteBoolean((bool)(underlyingValue ?? default(bool)));
+            else if (underlyingValueType == typeof(string))
+                bsonWriter.WriteString((string)(underlyingValue ?? string.Empty));
+            else if (underlyingValueType == typeof(decimal))
+                bsonWriter.WriteString(underlyingValue?.ToString() ?? default(decimal).ToString());
+        }
+
+        private object GetDeserializedValue(Type valueType, ref IBsonReader bsonReader)
+        {
+            var bsonType = bsonReader.CurrentBsonType;
+
+            if (bsonType == BsonType.Null)
+            {
+                bsonReader.ReadNull();
+                return null;
+            }
+            if (valueType == typeof(Guid))
+            {
+                var binaryData = bsonReader.ReadBinaryData();
+                return binaryData.ToGuid();
+            }
+            else if (valueType == typeof(double))
+                return bsonReader.ReadDouble();
+            else if (valueType == typeof(float))
+                return (float)bsonReader.ReadDouble();
+            else if (valueType == typeof(Int32))
+                return bsonReader.ReadInt32();
+            else if (valueType == typeof(Int64))
+                return bsonReader.ReadInt64();
+            else if (valueType == typeof(bool))
+                return bsonReader.ReadBoolean();
+            else if (valueType == typeof(string))
+                return bsonReader.ReadString();
+            else if (valueType == typeof(decimal))
+                return decimal.Parse(bsonReader.ReadString());
+
+            throw new Exception();
+        }
+    }
+}
+
+
+
+
+
+    /// <summary>
     /// 
     /// </summary>
-    public class ConceptSerializer : IBsonSerializer
+    /*public class ConceptSerializer : IBsonSerializer
     {
         /// <summary>
         /// Initializes a new instance of <see cref="ConceptSerializer"/>
@@ -128,4 +295,4 @@ namespace Infrastructure.Read.MongoDb
             throw new Exception();
         }
     }
-}
+}*/
