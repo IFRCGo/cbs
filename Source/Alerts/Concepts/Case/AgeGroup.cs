1f8615adf5d7792692e48c381590f456c65552e7
Bjørn Rasmussen <Bjorn@tradesolution.no>
2018-11-08 08:03:16 +0100

    #834: Added event for single case. Not that ageGroup and sex can be 0 if i.e. its a non human health risk

---
+++ Source/Alerts/Concepts/Case/AgeGroup.cs
@@ -0,0 +1,9 @@
+namespace Concepts.Case
+{
+    public enum AgeGroup
+    {
+        NotSet = 0,
+        AgeUnder5 = 1,
+        Age5AndOver = 2
+    }
+}
---
+++ Source/Alerts/Concepts/Case/Sex.cs
@@ -0,0 +1,9 @@
+namespace Concepts.Case
+{
+    public enum Sex
+    {
+        NotSet = 0,
+        Male = 1,
+        Female = 2
+    }
+}
