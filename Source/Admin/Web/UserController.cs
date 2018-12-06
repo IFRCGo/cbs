551d0ca2936e28e5075650a8d323064581ecdbe1
Kai Erland Thelin <kai.theline@online.no>
2018-12-06 19:24:52 +0100

    .

--- Source/Admin/Web/UserController.cs
+++ Source/Admin/Web/UserController.cs
@@ -1,12 +1,4 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) 2017-2018 The International Federation of Red Cross and Red Crescent Societies. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-/*---------------------------------------------------------------------------------------------
  *  Copyright (c) The International Federation of Red Cross and Red Crescent Societies. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
