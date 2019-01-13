ed8b7b00964da3a0513c2c960a58d9e90d4a29f6
Andrei Rusu <andrei.rusu@beatfactor.net>
2019-01-13 21:50:50 +0100

    Added React-based components for Reporting bounded context

---
+++ Source/VolunteerReporting/Web.Frontend/src/services/CaseReports.js
@@ -0,0 +1,12 @@
+import {BaseApi} from '@ifrc-cbs/common-react-ui';
+
+export default class CaseReports extends BaseApi {
+  fetchAllCaseReports(params) {
+    return this.fetch('/api/case-reports', {
+      parameters: {}
+    });
+  }
+};
+
+
+
