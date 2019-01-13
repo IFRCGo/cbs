8d7960238f52ebc8dbc4bfe592bef86e693e251b
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-01-13 15:33:52 +0100

    Add NotificationGateway in Docker Compose deployment

--- Deploy/Compose/all-build.sh
+++ Deploy/Compose/all-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f cbs-admin.yml -f cbs-reporting.yml -f cbs-usermanagement.yml build
+docker-compose -f cbs-admin.yml -f cbs-reporting.yml -f cbs-usermanagement.yml -f cbs-notificationgateway.yml build
