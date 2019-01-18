aa533a12292f423a5dc29848d7832d38fa4383b4
Endre Mikal Synnes <synend16@student.westerdals.no>
2019-01-18 19:15:18 +0100

    started on navgation for diagrams

--- Source/Analytics/Web.Frontend/src/index.js
+++ Source/Analytics/Web.Frontend/src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/Home";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <Route path="/" exact component={Home}/>
+        </App>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
