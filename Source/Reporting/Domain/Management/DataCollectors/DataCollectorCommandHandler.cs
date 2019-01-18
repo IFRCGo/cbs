71cca97fcddba6e61062aa2342e5ef4ffbf08797
Kai Erland Thelin <kai.theline@online.no>
2019-01-18 15:53:06 +0100

    Added input validation for begin and end training

--- Source/Reporting/Domain/Management/DataCollectors/DataCollectorCommandHandler.cs
+++ Source/Reporting/Domain/Management/DataCollectors/DataCollectorCommandHandler.cs
@@ -2,7 +2,7 @@
  *  Copyright (c) The International Federation of Red Cross and Red Crescent Societies. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-using System;
+
 using Dolittle.Domain;
 using Dolittle.Commands.Handling;
 
@@ -18,7 +18,5 @@ namespace Domain.Management.DataCollectors
         {
             _repository = repository;
         }
-        
-
     }
 }
--- Source/Reporting/Domain/Management/DataCollectors/Training/BeginTrainingInputValidator.cs
+++ Source/Reporting/Domain/Management/DataCollectors/Training/BeginTrainingInputValidator.cs
@@ -1,9 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) The International Federation of Red Cross and Red Crescent Societies. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Concepts.DataCollectors;
 using Dolittle.Commands.Validation;
+using FluentValidation;
 
 namespace Domain.Management.DataCollectors.Training
 {
     public class BeginTrainingInputValidator : CommandInputValidatorFor<BeginTraining>
     {
-        
+        public BeginTrainingInputValidator()
+        {
+            RuleFor(_ => _.DataCollectorId)
+                .NotEmpty().WithMessage("Data Collector Id must be set")
+                .SetValidator(new DataCollectorIdValidator());
+        }
     }
 }
