8336eef00917e63c6c0a4900fa6a7bee111ede80
Endre Mikal Synnes <synend16@student.westerdals.no>
2019-01-18 16:21:24 +0100

    started with some small components

--- Source/Analytics/Web.Frontend/src/App.js
+++ Source/Analytics/Web.Frontend/src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from "./components/sub/Header";
 
-class App extends Component {
-  render() {
+const App = ({children}) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+        <div>
+            <Header/>
+            {children}
+        </div>
+        )
+};
 
 export default App;
---
+++ Source/Analytics/Web.Frontend/src/components/sub/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="header">
+                <h1>Hello</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Header
