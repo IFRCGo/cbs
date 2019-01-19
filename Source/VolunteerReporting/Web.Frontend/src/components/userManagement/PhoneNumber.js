d1cf8dd592d6f38417c4e3a4593faa9b07e3384c
markuskr <mkrallinger@gmail.com>
2019-01-19 13:37:32 +0100

    Usermanagement in VolunteerReporting

---
+++ Source/VolunteerReporting/Web.Frontend/src/components/userManagement/PhoneNumber.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { TextInput, Label } from "evergreen-ui";
+
+export class PhoneNumber extends React.Component {
+    render() {
+        const {
+            label,
+            countryCodePlaceholder,
+            countryCodeValue,
+            numberPlaceholder,
+            numberValue,
+            onCountryCodeChange,
+            onPhoneNumberChange
+        } = this.props;
+        return (
+            <div className="phonenumber--container">
+                <Label>{label}</Label>
+                <div>
+                    <TextInput
+                        width="60px"
+                        marginRight="20px"
+                        className="phoneNumber--countryCode"
+                        placeholder={countryCodePlaceholder}
+                        value={countryCodeValue || ""}
+                        onChange={e =>
+                            !!onCountryCodeChange &&
+                            onCountryCodeChange(e.target.value)
+                        }
+                    />
+                    <TextInput
+                        width="200px"
+                        placeholder={numberPlaceholder}
+                        value={numberValue}
+                        onChange={e =>
+                            !!onPhoneNumberChange &&
+                            onPhoneNumberChange(e.target.value)
+                        }
+                    />
+                </div>
+            </div>
+        );
+    }
+}
