75d450da4c53d6c427599257d86fa1c5a63e54bb
Ginta Solyté (X) <a11710x@sb1a.sparebank1.no>
2019-01-19 15:14:01 +0100

    Map

--- Documentation/Projects/Analytics/Web Mockup/js/Mapbygeographicalarea.js
+++ Documentation/Projects/Analytics/Web Mockup/js/Mapbygeographicalarea.js
@@ -1,1 +1,66 @@
 ﻿
+// Prepare random data
+var data = [
+    ['DE.SH', 728],
+    ['DE.BE', 710],
+    ['DE.MV', 963],
+    ['DE.HB', 541],
+    ['DE.HH', 622],
+    ['DE.RP', 866],
+    ['DE.SL', 398],
+    ['DE.BY', 785],
+    ['DE.SN', 223],
+    ['DE.ST', 605],
+    ['DE.NW', 237],
+    ['DE.BW', 157],
+    ['DE.HE', 134],
+    ['DE.NI', 136],
+    ['DE.TH', 704],
+    ['DE.', 361]
+];
+
+$.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/germany.geo.json', function (geojson) {
+
+    // Initiate the chart
+    Highcharts.mapChart('container', {
+        chart: {
+            map: geojson
+        },
+       colors: ['rgba(19,64,117,0.0005)', 'rgba(19,64,117,0.2)', 'rgba(19,64,117,0.4)',
+                'rgba(19,64,117,0.5)', 'rgba(19,64,117,0.6)', 'rgba(19,64,117,0.8)', 'rgba(19,64,117,1)'],
+
+
+        title: {
+            text: 'Map by geographical area'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        colorAxis: {
+            tickPixelInterval: 100
+            
+        },
+        
+
+        series: [{
+            data: data,
+            keys: ['code_hasc', 'value'],
+            joinBy: 'code_hasc',
+            name: 'Number of cases',
+            states: {
+                hover: {
+                    color: '#a4edba'
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                format: '{point.properties.postal}'
+            }
+        }]
+    });
+});
