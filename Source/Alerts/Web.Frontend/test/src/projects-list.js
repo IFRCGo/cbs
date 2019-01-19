da7561c98afb740796d9ee5fe81381144907c562
Steven Hicks <Stevenah@users.noreply.github.com>
2019-01-19 14:30:00 +0100

    Replace reportoire with pure Redux, add Prettier formatting (#7)
    * added to package.json and prettier configuration
    
    * formated sourcee files
    
    * replacing reportoire (WIP)
    
    * redux boilerplate

--- Source/Alerts/Web.Frontend/test/src/projects-list.js
+++ Source/Alerts/Web.Frontend/test/src/projects-list.js
@@ -1,17 +1,17 @@
 module.exports = {
-  before(client) {
-    client.init();
-  },
+    before(client) {
+        client.init();
+    },
 
-  'Projects list is loaded successfully' (client) {
-    // TODO: make this testcase more expansive
-    client
-      .waitForElementVisible('header nav')
-      .click('header nav a[href="/projects/"]')
-      .waitForElementVisible('section.container table.table-striped');
-  },
+    'Projects list is loaded successfully'(client) {
+        // TODO: make this testcase more expansive
+        client
+            .waitForElementVisible('header nav')
+            .click('header nav a[href="/projects/"]')
+            .waitForElementVisible('section.container table.table-striped');
+    },
 
-  after(client) {
-    client.end();
-  }
+    after(client) {
+        client.end();
+    },
 };
