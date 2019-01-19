7ba877385eca3176449bb96b0e1da6647107f775
sigbjorn <sigbjorn.rudaa@gmail.com>
2019-01-19 11:58:53 +0100

    Set up initial backend with Michael the Ninja

---
+++ Source/Analytics/Read/CaseReports/CaseReportsEventProcessor.cs
@@ -0,0 +1,38 @@
+using Dolittle.Collections;
+using Dolittle.Events.Processing;
+using Dolittle.ReadModels;
+using Dolittle.Types;
+using Events.VolunteerReporting.CaseReports;
+
+namespace Read.CaseReports
+{
+    public class CaseReportsEventProcessor : ICanProcessEvents
+    {
+        private readonly IReadModelRepositoryFor<CaseReport> repository;
+        private readonly IInstancesOf<ICalculateFromCaseReports> calculators;
+
+        public CaseReportsEventProcessor(IReadModelRepositoryFor<CaseReport> repository, IInstancesOf<ICalculateFromCaseReports> calculators)
+        {
+            this.repository = repository;
+            this.calculators = calculators;
+        }
+
+        public void Process(CaseReportReceived @event)
+        {
+            var caseReport = new CaseReport{
+                NumberOfMalesAged5AndOlder = @event.NumberOfMalesAged5AndOlder,
+                NumberOfFemalesAged5AndOlder = @event.NumberOfFemalesAged5AndOlder,
+                NumberOfFemalesUnder5 = @event.NumberOfFemalesUnder5,
+                NumberOfMalesUnder5 = @event.NumberOfMalesUnder5,
+                Timestamp = @event.Timestamp,
+                HealthRisk = @event.HealthRiskId,
+                Longitude = @event.Longitude,
+                Latitude = @event.Latitude
+            };
+            this.repository.Insert(caseReport);
+
+            this.calculators.ForEach(c => c.Calculate(caseReport));
+        }
+    }
+
+}
