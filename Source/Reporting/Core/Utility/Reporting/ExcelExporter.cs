aa7046280120bdcc1c44c6c3e3cdbdee8f4dcdfc
Espen Johnsen <powerpoint@gmail.com>
2019-01-19 15:29:39 +0100

    Rewrote excel-export with ClosedXML and removed OpenDocument-XML.

--- Source/Reporting/Core/Utility/Reporting/ExcelExporter.cs
+++ Source/Reporting/Core/Utility/Reporting/ExcelExporter.cs
@@ -6,11 +6,100 @@ using Read.Reporting.CaseReportsForListing;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DocumentFormat.OpenXml;
+using ClosedXML.Excel;
+using System.Globalization;
 
 namespace Core.Utility
 {
     public class ExcelExporter : IReportExporter
     {
+        List<ReportColumn> columns;
+        IXLWorkbook wb;
+        IXLWorksheet ws;
+
+        public ExcelExporter()
+        {
+            columns = new List<ReportColumn>();
+            wb = new XLWorkbook();
+            ws = wb.Worksheets.Add("Report " + DateTime.Now.ToString("yyyy-MM-dd HH꞉mm")); // NOTE: The colon is not a normal column, but a unicode because of excel rules
+
+            columns.Add(new ReportColumn("A", ColumnType.Date, "Date"));
+            columns.Add(new ReportColumn("B", ColumnType.Time, "Time"));
+            columns.Add(new ReportColumn("C", ColumnType.EpiWeek, "Week"));
+            columns.Add(new ReportColumn("D", ColumnType.Status, "Status"));
+            columns.Add(new ReportColumn("E", ColumnType.DataCollector, "Data Collector"));
+            columns.Add(new ReportColumn("F", ColumnType.Region, "Region"));
+            columns.Add(new ReportColumn("G", ColumnType.District, "District"));
+            columns.Add(new ReportColumn("H", ColumnType.Village, "Village"));
+            columns.Add(new ReportColumn("I", ColumnType.HealthRisk, "Health risk"));
+            columns.Add(new ReportColumn("J", ColumnType.MalesUnder5, "Males under 5"));
+            columns.Add(new ReportColumn("K", ColumnType.Males5AndOlder, "Males 5 and older"));
+            columns.Add(new ReportColumn("L", ColumnType.FemalesUnder5, "Females under 5"));
+            columns.Add(new ReportColumn("M", ColumnType.Females5AndOlder, "Females 5 and older"));
+
+            columns.Add(new ReportColumn("N", ColumnType.TotalUnder5, "Under 5 Total"));
+            columns.Add(new ReportColumn("O", ColumnType.Total5OrOlder, "5 and older Total"));
+
+            columns.Add(new ReportColumn("P", ColumnType.Lat, "Lat"));
+            columns.Add(new ReportColumn("Q", ColumnType.Long, "Long"));
+            columns.Add(new ReportColumn("R", ColumnType.Message, "Message"));
+            columns.Add(new ReportColumn("S", ColumnType.ErrorMessage, "Errors"));
+        }
+
+        public bool WriteReports(IEnumerable<CaseReportForListing> reports, string[] fields, Stream stream)
+        {
+            uint rowIndex = 1;
+
+            foreach (var column in columns)
+            {
+                ws.Cell(column.ExcelColumnName + rowIndex).Value = column.HeaderName;
+                ws.Cell(column.ExcelColumnName + rowIndex).Style.Font.Bold = true;
+            }
+
+            foreach (var report in reports.OrderByDescending(e => e.Timestamp))
+            {
+                rowIndex++;
+
+                GetCell(ColumnType.Date, rowIndex).Value = report.Timestamp;
+                GetCell(ColumnType.Date, rowIndex).Style.DateFormat.Format = "dd/mm/yyy";
+
+                GetCell(ColumnType.Time, rowIndex).Value = report.Timestamp;
+                GetCell(ColumnType.Time, rowIndex).Style.DateFormat.Format = "HH:MM:ss";
+
+                GetCell(ColumnType.EpiWeek, rowIndex).Value = GetEpiWeek(report.Timestamp.DateTime);
+
+                GetCell(ColumnType.Status, rowIndex).Value = report.HealthRiskId != null ? "Success" : "Error";
+                GetCell(ColumnType.DataCollector, rowIndex).Value = report.DataCollectorId != null ? report.DataCollectorDisplayName : "Origin: " + report.Origin;
+                GetCell(ColumnType.Region, rowIndex).Value = report.DataCollectorId != null ? report.DataCollectorRegion : "";
+                GetCell(ColumnType.District, rowIndex).Value = report.DataCollectorId != null ? report.DataCollectorDistrict : "";
+                GetCell(ColumnType.Village, rowIndex).Value = report.DataCollectorId != null ? report.DataCollectorVillage : "";
+                GetCell(ColumnType.Message, rowIndex).Value = report.Message;
+
+                if (report.HealthRiskId != null)
+                {
+                    GetCell(ColumnType.HealthRisk, rowIndex).Value = report.HealthRisk;
+                    GetCell(ColumnType.MalesUnder5, rowIndex).Value = report.NumberOfMalesUnder5;
+                    GetCell(ColumnType.Males5AndOlder, rowIndex).Value = report.NumberOfMalesAged5AndOlder;
+                    GetCell(ColumnType.FemalesUnder5, rowIndex).Value = report.NumberOfFemalesUnder5;
+                    GetCell(ColumnType.Females5AndOlder, rowIndex).Value = report.NumberOfFemalesAged5AndOlder;
+
+                    GetCell(ColumnType.Lat, rowIndex).Value = report.Location?.Latitude;
+                    GetCell(ColumnType.Long, rowIndex).Value = report.Location?.Longitude;
+
+                    GetCell(ColumnType.TotalUnder5, rowIndex).FormulaA1 = $"={GetCellAddress(ColumnType.MalesUnder5)}{rowIndex}+{GetCellAddress(ColumnType.FemalesUnder5)}{rowIndex}";
+                    GetCell(ColumnType.Total5OrOlder, rowIndex).FormulaA1 = $"={GetCellAddress(ColumnType.Males5AndOlder)}{rowIndex}+{GetCellAddress(ColumnType.Females5AndOlder)}{rowIndex}";
+                }
+                else
+                {
+                    GetCell(ColumnType.ErrorMessage, rowIndex).Value = report.ParsingErrorMessage != null ? string.Join(".", report.ParsingErrorMessage) : "Health Risk Error";
+                }
+            }
+            ws.Columns().AdjustToContents(); // Adjust width of all columns to content
+            ws.RangeUsed().SetAutoFilter();
+            wb.SaveAs(stream);
+            return true;
+        }
+
         public string GetFileExtension()
         {
             return ".xlsx";
@@ -21,155 +110,66 @@ namespace Core.Utility
             return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
         }
 
-        public bool WriteReports(IEnumerable<CaseReportForListing> reports, string[] fields, Stream stream)
+        enum ColumnType
         {
-            var document = SpreadsheetDocument.Create(stream, SpreadsheetDocumentType.Workbook);
-            WorkbookPart workbook = document.AddWorkbookPart();
-            workbook.Workbook = new Workbook();
-            Sheets sheets = document.WorkbookPart.Workbook.AppendChild(new Sheets());
+            Date,
+            Time,
+            EpiWeek,
+            Status,
+            DataCollector,
+            Region,
+            District,
+            Village,
+            HealthRisk,
+            MalesUnder5,
+            Males5AndOlder,
+            FemalesUnder5,
+            Females5AndOlder,
+            Lat,
+            Long,
+            Message,
+            ErrorMessage,
+            TotalUnder5,
+            Total5OrOlder
+        }
 
-            // Create a sheet in the document
-            var data = new SheetData();
-            WorksheetPart worksheet = workbook.AddNewPart<WorksheetPart>();
-            worksheet.Worksheet = new Worksheet(data);
-            Sheet sheet = new Sheet() { Id = document.WorkbookPart.GetIdOfPart(worksheet), SheetId = 1, Name = "Case Reports" };
-            sheets.Append(sheet);
 
+        private string GetCellAddress(ColumnType type)
+        {
+            return columns.Single(c => c.ColumnType == type).ExcelColumnName;
+        }
 
-            uint rowIndex = 0;
-            // Add some headers
+        private IXLCell GetCell(ColumnType type, uint rowIndex)
+        {
+            var excelColumnName = GetCellAddress(type);
+            return ws.Cell(excelColumnName + rowIndex);
+        }
+
+        public static int GetEpiWeek(DateTime time)
+        {
+            if (time.Month == 12 && time.Day > 28)
             {
-                var row = new Row { RowIndex = ++rowIndex };
-                data.Append(row);
-
-                var headers = new SortedDictionary<string, string>
-                    {
-                        { "A", "Date" },
-                        { "B", "Time"},
-                        { "C", "Status" },
-                        { "D", "Data Collector" },
-                        { "E", "Region" },
-                        { "F", "District" },
-                        { "G", "Village" },
-                        { "H", "Health Risk" },
-                        { "I", "Males < 5" },
-                        { "J", "Males ≥ 5" },
-                        { "K", "Females < 5" },
-                        { "L", "Females ≥ 5" },
-                        { "M", "Lat. / Long." },
-                        { "N", "Message" },
-                        { "O", "Errors" }
-                    };
-
-                foreach (var header in headers)
+                DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+                if (day >= DayOfWeek.Sunday && day <= DayOfWeek.Tuesday)
                 {
-                    var cell = new Cell { CellReference = header.Key + rowIndex };
-                    row.Append(cell);
-                    cell.DataType = new EnumValue<CellValues>(CellValues.String);
-                    cell.CellValue = new CellValue(header.Value);
+                    time = time.AddDays(3);
                 }
             }
 
-            // Insert data
-            foreach (var report in reports.OrderByDescending(e => e.Timestamp))
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
+        }
+
+        class ReportColumn
+        {
+            public string HeaderName { get; set; }
+            public string ExcelColumnName { get; set; }
+            public ColumnType ColumnType { get; set; }
+            public ReportColumn(string excelColumn, ColumnType type, string header)
             {
-                var row = new Row { RowIndex = ++rowIndex };
-                data.Append(row);
-
-                var date = new Cell { CellReference = "A" + rowIndex };
-                row.Append(date);
-                date.DataType = new EnumValue<CellValues>(CellValues.Date);
-                date.CellValue = new CellValue(report.Timestamp.ToString("yyyy MMMM dd"));
-
-                var timestamp = new Cell { CellReference = "B" + rowIndex };
-                row.Append(timestamp);
-                timestamp.DataType = new EnumValue<CellValues>(CellValues.String);
-                timestamp.CellValue = new CellValue(report.Timestamp.ToString("HH:mm:ss"));
-
-                var status = new Cell { CellReference = "C" + rowIndex };
-                row.Append(status);
-                status.DataType = new EnumValue<CellValues>(CellValues.String);
-
-                var origin = new Cell { CellReference = "D" + rowIndex };
-                row.Append(origin);
-                origin.DataType = new EnumValue<CellValues>(CellValues.String);
-                origin.CellValue = new CellValue(report.DataCollectorId != null ? report.DataCollectorDisplayName : "Origin: " + report.Origin);
-
-                var region = new Cell { CellReference = "E" + rowIndex };
-                row.Append(region);
-                region.DataType = new EnumValue<CellValues>(CellValues.String);
-                region.CellValue = new CellValue(report.DataCollectorId != null ? report.DataCollectorRegion : "");
-
-                var district = new Cell { CellReference = "F" + rowIndex };
-                row.Append(district);
-                district.DataType = new EnumValue<CellValues>(CellValues.String);
-                district.CellValue = new CellValue(report.DataCollectorId != null ? report.DataCollectorDistrict : "");
-
-                var village = new Cell { CellReference = "G" + rowIndex };
-                row.Append(village);
-                village.DataType = new EnumValue<CellValues>(CellValues.String);
-                village.CellValue = new CellValue(report.DataCollectorId != null ? report.DataCollectorVillage : "");
-
-                var healthrisk = new Cell { CellReference = "H" + rowIndex };
-                row.Append(healthrisk);
-                healthrisk.DataType = new EnumValue<CellValues>(CellValues.String);
-
-                var malesUnder5 = new Cell { CellReference = "I" + rowIndex };
-                row.Append(malesUnder5);
-                malesUnder5.DataType = new EnumValue<CellValues>(CellValues.Number);
-
-                var malesOver5 = new Cell { CellReference = "J" + rowIndex };
-                row.Append(malesOver5);
-                malesOver5.DataType = new EnumValue<CellValues>(CellValues.Number);
-
-                var femalesUnder5 = new Cell { CellReference = "K" + rowIndex };
-                row.Append(femalesUnder5);
-                femalesUnder5.DataType = new EnumValue<CellValues>(CellValues.Number);
-
-                var femalesOver5 = new Cell { CellReference = "L" + rowIndex };
-                row.Append(femalesOver5);
-                femalesOver5.DataType = new EnumValue<CellValues>(CellValues.Number);
-
-                var location = new Cell { CellReference = "M" + rowIndex };
-                row.Append(location);
-                location.DataType = new EnumValue<CellValues>(CellValues.String);
-                location.CellValue = new CellValue(report.Location != null ? report.Location.Latitude + "/" + report.Location.Longitude : "");
-
-                var message = new Cell { CellReference = "N" + rowIndex };
-                row.Append(message);
-                message.DataType = new EnumValue<CellValues>(CellValues.String);
-                message.CellValue = new CellValue(report.Message);
-
-                var error = new Cell { CellReference = "O" + rowIndex };
-                row.Append(error);
-                error.DataType = new EnumValue<CellValues>(CellValues.String);
-
-                if (report.HealthRiskId != null)
-                {
-                    status.CellValue = new CellValue("Success");
-                    healthrisk.CellValue = new CellValue(report.HealthRisk);
-                    malesUnder5.CellValue = new CellValue(report.NumberOfMalesUnder5.ToString());
-                    malesOver5.CellValue = new CellValue(report.NumberOfMalesAged5AndOlder.ToString());
-                    femalesUnder5.CellValue = new CellValue(report.NumberOfFemalesUnder5.ToString());
-                    femalesOver5.CellValue = new CellValue(report.NumberOfFemalesAged5AndOlder.ToString());
-                    error.CellValue = new CellValue("");
-                }
-                else
-                {
-                    status.CellValue = new CellValue("Error");
-                    healthrisk.CellValue = new CellValue("");
-                    malesUnder5.CellValue = new CellValue("");
-                    malesOver5.CellValue = new CellValue("");
-                    femalesUnder5.CellValue = new CellValue("");
-                    femalesOver5.CellValue = new CellValue("");
-                    error.CellValue = new CellValue(report.ParsingErrorMessage != null ? string.Join(".", report.ParsingErrorMessage) : "");
-                }
+                HeaderName = header;
+                ColumnType = type;
+                ExcelColumnName = excelColumn;
             }
-
-            // Save the document in memory, and serve to client
-            workbook.Workbook.Save();
-            document.Close();
-            return true;
         }
     }
 }
