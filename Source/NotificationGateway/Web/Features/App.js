a430d73e5a2fc8d25865ea321e7a0638ef67e7d6
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-02-24 00:20:59 +0100

    Implement form for simulating messages in React

--- Source/NotificationGateway/Web/Features/App.js
+++ Source/NotificationGateway/Web/Features/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
 import {CommandCoordinator} from '@dolittle/commands';
 import {QueryCoordinator} from '@dolittle/queries';
+
+import Messages from './SMS/Messages';
 
 CommandCoordinator.apiBaseUrl = process.env.API_BASE_URL;
 QueryCoordinator.apiBaseUrl = process.env.API_BASE_URL;
@@ -9,7 +10,10 @@ QueryCoordinator.apiBaseUrl = process.env.API_BASE_URL;
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello NotificationGateway!</h1>
+      <React.Fragment>
+        <h1>Notification gateway</h1>
+        <Messages />
+      </React.Fragment>
     );
   }
 }
