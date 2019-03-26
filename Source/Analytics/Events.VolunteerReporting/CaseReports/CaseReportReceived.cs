f9bbaacef6593a96d58850e267eb92e044141e30
Karoline Klever <karolikl@gmail.com>
2019-03-26 10:06:26 +0100

    Added Events from VolunteerReporting

---
+++ Source/Analytics/Events.VolunteerReporting/CaseReports/CaseReportReceived.cs
@@ -0,0 +1,43 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2017 International Federation of Red Cross. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+using System;
+using Dolittle.Events;
+
+namespace Events.VolunteerReporting.CaseReports
+{
+    public class CaseReportReceived : IEvent
+    {
+        public CaseReportReceived(Guid caseReportId, Guid dataCollectorId, Guid healthRiskId,
+            string origin, string message, int numberOfMalesUnder5, int numberOfMalesAged5AndOlder,
+            int numberOfFemalesUnder5, int numberOfFemalesAged5AndOlder, double longitude,
+            double latitude, DateTimeOffset timestamp)
+        {
+            this.CaseReportId = caseReportId;
+            this.DataCollectorId = dataCollectorId;
+            this.HealthRiskId = healthRiskId;
+            this.Origin = origin;
+            this.Message = message;
+            this.NumberOfMalesUnder5 = numberOfMalesUnder5;
+            this.NumberOfMalesAged5AndOlder = numberOfMalesAged5AndOlder;
+            this.NumberOfFemalesUnder5 = numberOfFemalesUnder5;
+            this.NumberOfFemalesAged5AndOlder = numberOfFemalesAged5AndOlder;
+            this.Longitude = longitude;
+            this.Latitude = latitude;
+            this.Timestamp = timestamp;
+        }
+        public Guid CaseReportId { get; }
+        public Guid DataCollectorId { get; }
+        public Guid HealthRiskId { get; }
+        public string Origin { get; }
+        public string Message { get; }
+        public int NumberOfMalesUnder5 { get; }
+        public int NumberOfMalesAged5AndOlder { get; }
+        public int NumberOfFemalesUnder5 { get; }
+        public int NumberOfFemalesAged5AndOlder { get; }
+        public double Longitude { get; }
+        public double Latitude { get; }
+        public DateTimeOffset Timestamp { get; }
+    }
+}
