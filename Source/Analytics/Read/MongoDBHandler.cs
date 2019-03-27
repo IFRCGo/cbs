7dcaa124ea9942b85ddb5f0f40ceb5f51da765da
Dan Morris <damorri@microsoft.com>
2019-03-27 13:46:43 +0100

    added queryable and parameterized mongodbhandler

--- Source/Analytics/Read/MongoDBHandler.cs
+++ Source/Analytics/Read/MongoDBHandler.cs
@@ -7,15 +7,23 @@ namespace Read
 {
     public class MongoDBHandler
     {
-        private readonly string connectionString = "mongodb://localhost";
+        private string connectionString = "mongodb://localhost";
+        private string databaseName = "read_model_database";
 
         public MongoDBHandler()
-        { }
+        {
+        }
 
-        private void SetUpConnectionAsync()
+        public MongoDBHandler(string connectionString, string databaseName)
+        {
+            this.connectionString = connectionString;
+            this.databaseName = databaseName;
+        }
+
+        private void setUpConnectionAsync()
         {
             MongoClient client = new MongoClient(connectionString);
-            IMongoDatabase database = client.GetDatabase("read_model_database");
+            IMongoDatabase database = client.GetDatabase(this.databaseName);
             var collection = database.GetCollection<BsonDocument>("CaseReport");
             var list = collection.Find(new BsonDocument()).ToEnumerable();
 
@@ -25,25 +33,15 @@ namespace Read
             }
         }
 
-        public IQueryable<DbCaseEntry> GetQueryable()
+        public IQueryable<DbCaseEntry> getQueryable()
         {
             MongoClient client = new MongoClient(connectionString);
-            IMongoDatabase database = client.GetDatabase("read_model_database");
+            IMongoDatabase database = client.GetDatabase(this.databaseName);
             var collection = database.GetCollection<DbCaseEntry>("CaseReport");
             return collection.AsQueryable().AsQueryable();
         }
 
-        public void DeleteAllRecordsFromDB()
-        {
-            var client = new MongoClient(connectionString);
-
-            //Use the MongoClient to access the server
-            IMongoDatabase database = client.GetDatabase("read_model_database");
-            database.DropCollection("CaseReport");
-            database.DropCollection("DataOwners");
-        }
-
-        public void InsertRecordToDB(DbCaseEntry dbEntry)
+        public void insertRecordToDB(DbCaseEntry dbEntry)
         {
             // Create a MongoClient object by using the connection string
             var client = new MongoClient(connectionString);
