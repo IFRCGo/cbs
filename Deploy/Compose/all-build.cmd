237237bd11e06adbfd3ecd0388a6116db86ab28a
Karoline Klever <karolikl@gmail.com>
2019-03-27 09:42:20 +0100

    Docker compose for Analytics

--- Deploy/Compose/all-build.cmd
+++ Deploy/Compose/all-build.cmd
@@ -1,1 +1,1 @@
-docker-compose -f cbs-admin.yml -f cbs-reporting.yml -f cbs-notificationgateway.yml -f cbs-alerts.yml build
+docker-compose -f cbs-admin.yml -f cbs-reporting.yml -f cbs-notificationgateway.yml -f cbs-alerts.yml -f cbs-analytics.yml build
