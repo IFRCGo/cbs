494d32eae3b5e43c50282ae2bc2b2cf6cdee8b37
Karoline Klever <karolikl@gmail.com>
2019-03-28 12:06:54 +0100

    Added update of healthrisk reports

--- Source/Analytics/Read/HealthRisks/HealthRisk.cs
+++ Source/Analytics/Read/HealthRisks/HealthRisk.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Read.HealthRisks
 {
@@ -9,10 +9,23 @@ namespace Read.HealthRisks
         public Guid HealthRiskId { get; set; }
         public string Name { get; set; }
 
+        public Dictionary<int, int> ReportsPerWeek { get; set; }
+
         public HealthRisk(Guid id, string name)
         {
             HealthRiskId = id;
             Name = name;
+            ReportsPerWeek = new Dictionary<int, int>();
+        }
+
+        public void ReportReceived(DateTimeOffset dateTime)
+        {
+            int weekNumber = dateTime.DateTime.GetWeekNumber();
+
+            if (ReportsPerWeek.Keys.Contains(weekNumber))
+                ReportsPerWeek[weekNumber] = ReportsPerWeek[weekNumber] + 1;
+            else
+                ReportsPerWeek[weekNumber] = 1;
         }
     }
 }
--- Source/Analytics/Web/Controllers/AnalysisService.cs
+++ Source/Analytics/Web/Controllers/AnalysisService.cs
@@ -59,20 +59,8 @@ namespace Web.Controllers
             {
                 return date.ToShortDateString();
             }
-
-            return $"Week {GetIso8601WeekOfYear(date)}";
-        }
-
-        public static int GetIso8601WeekOfYear(DateTime time)
-        {
-            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
-            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
-            {
-                time = time.AddDays(3);
-            }
-
-            // Return the week of our adjusted day
-            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            
+            return $"Week {date.GetWeekNumber()}";
         }
 
         private Serie[] GetSeries(IEnumerable<IGrouping<string, CaseReport>> groups, SelectedSeries[] selectedSeries)
