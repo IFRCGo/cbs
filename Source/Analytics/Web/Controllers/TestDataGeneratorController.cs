f90c562e43d584a478f34fbba040e9b34f466921
Karoline Klever <karolikl@gmail.com>
2019-03-28 12:32:50 +0100

    Added generation of healthrisks in testdatagenerator

--- Source/Analytics/Web/Controllers/TestDataGeneratorController.cs
+++ Source/Analytics/Web/Controllers/TestDataGeneratorController.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using Web.TestData;
 using Read;
 using Read.CaseReports;
+using Read.HealthRisks;
 
 namespace Web.Controllers
 {
@@ -13,55 +14,41 @@ namespace Web.Controllers
     [ApiController]
     public class TestDataGeneratorController : ControllerBase
     {
+        private readonly ICaseReportsEventHandler _caseReportsEventHandler;
+        private readonly IHealthRisksEventHandler _healthRisksEventHandler;
         private readonly MongoDBHandler _mongoDbHandler;
-        public TestDataGeneratorController(MongoDBHandler mongoDbHandler){
+
+        public TestDataGeneratorController(ICaseReportsEventHandler caseReportsEventHandler,
+            IHealthRisksEventHandler healthRisksEventHandler,
+            MongoDBHandler mongoDbHandler)
+  
+        {
+            _caseReportsEventHandler = caseReportsEventHandler;
+            _healthRisksEventHandler = healthRisksEventHandler;
             _mongoDbHandler = mongoDbHandler;
         }
 
-        [HttpGet("{daysToGenerate}", Name="GenerateTestData")]
+        [HttpGet("{daysToGenerate}", Name= "All")]
         public ActionResult GenerateTestData(int daysToGenerate)
         {
-            var maxNumberOfReportsDaily = 10;
-            var maxNumberOfMalesUnderFive = 40;
-            var maxNumberOfMalesFiveOrAbove = 100;
-            var maxNumberOfFemalesUnderFive = 40;
-            var maxNumberOfFemalesFiveOrAbove = 100;
+            var healthRisks = new Dictionary<Guid, string>();
+            healthRisks.Add(new Guid("c8fc3010-05d7-49b6-9ad0-97d83a3bfe59"), "Cholera");
+            healthRisks.Add(new Guid("1bbe74fa-a1f4-4204-9cc4-9e8190a1184f"), "Acute Watery Diarrhea");
+            healthRisks.Add(new Guid("8eabbdad-7f01-43ff-9ed0-bba1b00df051"), "Measels");
 
-            Random rnd = new Random();
-            for (int dayOffset = daysToGenerate; dayOffset >= 0; dayOffset--)
-            {
-                var numberOfDailyReports = rnd.Next(maxNumberOfReportsDaily);
-                for (int j = 0; j < numberOfDailyReports; j++)
-                {
-                    var dbCaseEntry = new CaseReport(
-                        Guid.NewGuid(),
-                        Guid.NewGuid(),
-                        Guid.NewGuid(),
-                        "phoneNumber",
-                        "test message",
-                        rnd.Next(maxNumberOfMalesUnderFive),
-                        rnd.Next(maxNumberOfMalesFiveOrAbove),
-                        rnd.Next(maxNumberOfFemalesUnderFive),
-                        rnd.Next(maxNumberOfFemalesFiveOrAbove),
-                        rnd.Next(-180, 180),
-                        rnd.Next(-90, 90),
-                        DateTimeOffset.UtcNow.AddDays(-dayOffset));
-
-                    _mongoDbHandler.Insert(dbCaseEntry);
-                }
-            }
+            GenerateHealthRisks(healthRisks);
+            GenerateCaseReports(daysToGenerate, healthRisks);
 
             return Ok();
         }
 
-        // GET api/TestData
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> GenerateTestData()
+        [HttpGet(Name = "CaseReports")]
+        public ActionResult<IEnumerable<string>> CaseReports()
         {
             var caseReports = JsonConvert.DeserializeObject<CaseReport[]>(System.IO.File.ReadAllText("./TestData/CaseReports.json"));
             foreach (var caseReport in caseReports)
             {
-                var dbCaseEntry = new CaseReport(
+                var report = new CaseReport(
                     Guid.NewGuid(),
                     caseReport.DataCollectorId,
                     caseReport.HealthRisk,
@@ -75,14 +62,41 @@ namespace Web.Controllers
                     caseReport.Latitude,
                     DateTimeOffset.UtcNow);
 
-                _mongoDbHandler.Insert(dbCaseEntry);
+                _caseReportsEventHandler.Handle(report);
+            }
+
+            return caseReports.Select(x => x.Message).ToArray();
+        }
+
+
+        [HttpGet(Name = "HealthRisks")]
+        public ActionResult<IEnumerable<string>> HealthRisks()
+        {
+            var caseReports = JsonConvert.DeserializeObject<CaseReport[]>(System.IO.File.ReadAllText("./TestData/CaseReports.json"));
+            foreach (var caseReport in caseReports)
+            {
+                var report = new CaseReport(
+                    Guid.NewGuid(),
+                    caseReport.DataCollectorId,
+                    caseReport.HealthRisk,
+                    caseReport.Origin,
+                    caseReport.Message,
+                    caseReport.NumberOfMalesUnder5,
+                    caseReport.NumberOfMalesAged5AndOlder,
+                    caseReport.NumberOfFemalesUnder5,
+                    caseReport.NumberOfFemalesAged5AndOlder,
+                    caseReport.Longitude,
+                    caseReport.Latitude,
+                    DateTimeOffset.UtcNow);
+
+                _caseReportsEventHandler.Handle(report);
             }
 
             return caseReports.Select(x => x.Message).ToArray();
         }
 
         // GET api/TestData/dataowner
-        [HttpGet("dataowner")]
+        [HttpGet("DataCollectors")]
         public ActionResult<IEnumerable<string>> GenerateTestDataOwner()
         {
             var dataOwners = JsonConvert.DeserializeObject<DataOwners[]>(System.IO.File.ReadAllText("TestData/DataOwners.json"));
@@ -97,14 +111,6 @@ namespace Web.Controllers
             return dataOwners.Select(x => x.Name).ToArray();
         }
 
-        // GET api/TestData/delete
-        [HttpGet("delete")]
-        public string DeleteTestData()
-        {
-            // _mongoDbHandler.DeleteAllRecordsFromDB();
-            return "test";
-        }
-
         // GET api/TestData/{dataowner}
         [HttpGet("guid")]
         public string FetchTestDataOwner()
@@ -112,6 +118,48 @@ namespace Web.Controllers
             var testDataService = new TestDataService(_mongoDbHandler);
             var guid = new Guid("a987c35c-1c7a-4bd3-a449-a071ffeb71e7");
             return testDataService.GetDataOwner(guid);
+        }
+
+        private void GenerateHealthRisks(Dictionary<Guid, string> healthRisks)
+        {
+            foreach(var keyValuePair in healthRisks)
+            {
+                var healthRisk = new HealthRisk(keyValuePair.Key, keyValuePair.Value);
+                _healthRisksEventHandler.Handle(healthRisk);
+            }
+        }
+
+            private void GenerateCaseReports(int daysToGenerate, Dictionary<Guid, string> healthRisks)
+        {
+            var maxNumberOfReportsDaily = 10;
+            var maxNumberOfMalesUnderFive = 40;
+            var maxNumberOfMalesFiveOrAbove = 100;
+            var maxNumberOfFemalesUnderFive = 40;
+            var maxNumberOfFemalesFiveOrAbove = 100;
+
+            Random rnd = new Random();
+            for (int dayOffset = daysToGenerate; dayOffset >= 0; dayOffset--)
+            {
+                var numberOfDailyReports = rnd.Next(maxNumberOfReportsDaily);
+                for (int j = 0; j < numberOfDailyReports; j++)
+                {
+                    var caseReport = new CaseReport(
+                        Guid.NewGuid(),
+                        Guid.NewGuid(),
+                        healthRisks.ElementAt(rnd.Next(0, healthRisks.Count)).Key,
+                        "phoneNumber",
+                        "test message",
+                        rnd.Next(maxNumberOfMalesUnderFive),
+                        rnd.Next(maxNumberOfMalesFiveOrAbove),
+                        rnd.Next(maxNumberOfFemalesUnderFive),
+                        rnd.Next(maxNumberOfFemalesFiveOrAbove),
+                        rnd.Next(-180, 180),
+                        rnd.Next(-90, 90),
+                        DateTimeOffset.UtcNow.AddDays(-dayOffset));
+
+                    _caseReportsEventHandler.Handle(caseReport);
+                }
+            }
         }
     }
 }
