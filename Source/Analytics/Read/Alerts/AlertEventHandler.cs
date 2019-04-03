e3c515bac22a473962f139a6d710b1a290936d66
Karoline Klever <karolikl@gmail.com>
2019-04-03 18:36:01 +0200

    Readded AgeAndSexDistribution.js as this disappeared at some point

---
+++ Source/Analytics/Read/Alerts/AlertEventHandler.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Read.Alerts
+{
+    public class AlertEventHandler : IAlertEventHandler
+    {
+        public void Handle(Alert @event)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
