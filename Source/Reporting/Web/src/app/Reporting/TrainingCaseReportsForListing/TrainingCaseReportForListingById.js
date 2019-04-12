a7dc66cc3e6386f1a6ee417ec27a1717796a9382
Sindre Wilting <sindrewilting@gmail.com>
2019-04-12 15:14:43 +0000

    proxies

---
+++ Source/Reporting/Web/src/app/Reporting/TrainingCaseReportsForListing/TrainingCaseReportForListingById.js
@@ -0,0 +1,16 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Query Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Query } from  '@dolittle/queries';
+
+export class TrainingCaseReportForListingById extends Query
+{
+    constructor() {
+        super();
+        this.nameOfQuery = 'TrainingCaseReportForListingById';
+        this.generatedFrom = 'Read.Reporting.TrainingCaseReportsForListing.TrainingCaseReportForListingById';
+
+        this.caseReportId = '00000000-0000-0000-0000-000000000000';
+    }
+}
