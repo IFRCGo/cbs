48c3f93ddc996b2e531c4bf40809e9a10da5ba20
rukundobertrand <rukundobertrand@gmail.com>
2019-04-12 10:36:48 +0000

    Added test data for Alert rules

--- Source/Alerts/Web/mocking/MockQueryCoordinator.js
+++ Source/Alerts/Web/mocking/MockQueryCoordinator.js
@@ -36,10 +36,32 @@ export class MockQueryCoordinator {
             rule1.numberOfCasesThreshold = 4;
             rule1.distanceBetweenCasesInMeters = 1000;
             rule1.thresholdTimeframeInHours = 24;
+            let rule2 = new AlertRule();
+            rule2.id = 'c55ff00f-fed9-4ba0-a270-c182464fc99d';
+            rule2.alertRuleName = 'Ebola';
+            rule2.healthRiskId = 2;
+            rule2.numberOfCasesThreshold = 1;
+            rule2.distanceBetweenCasesInMeters = 0;
+            rule2.thresholdTimeframeInHours = 24;
+            let rule3 = new AlertRule();
+            rule3.id = 'c55ff00f-fed9-4ba0-a380-c1824574fc99d';
+            rule3.alertRuleName = 'Yellow Fever';
+            rule3.healthRiskId = 3;
+            rule3.numberOfCasesThreshold = 1;
+            rule3.distanceBetweenCasesInMeters = 0;
+            rule3.thresholdTimeframeInHours = 24;
+            let rule4 = new AlertRule();
+            rule4.id = 'd66aa03f-fce9-34fr-fgr6-se4r56td3456';
+            rule4.alertRuleName = 'Measles';
+            rule4.healthRiskId = 4;
+            rule4.numberOfCasesThreshold = 1;
+            rule4.distanceBetweenCasesInMeters = 0;
+            rule4.thresholdTimeframeInHours = 24;
             let items = [
-                rule1                
+                rule1,rule2,rule3,rule4                
             ];            
             return new QueryResult(query, items);
+            
         }
         else if(query instanceof GetDataOwner){
            let dataOwner = new DataOwner();
