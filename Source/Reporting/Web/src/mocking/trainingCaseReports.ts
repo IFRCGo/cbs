53c43ce98dbad428db5e27b030d26970c393aaa4
Sindre Wilting <sindrewilting@gmail.com>
2019-04-12 15:30:16 +0000

    Better mock data

--- Source/Reporting/Web/src/mocking/trainingCaseReports.ts
+++ Source/Reporting/Web/src/mocking/trainingCaseReports.ts
@@ -1,12 +1,12 @@
-import { CaseReportForListing} from '../app/Reporting/CaseReportsForListing/CaseReportForListing';
+import { TrainingCaseReportForListing} from '../app/Reporting/TrainingCaseReportsForListing/TrainingCaseReportForListing';
 import {Guid} from '@dolittle/core';
 import dataCollectors from './dataCollectors';
 
-let caseReports : CaseReportForListing[] = [];
+let trainingCaseReports : TrainingCaseReportForListing[] = [];
 for(let i = 0; i < 5; i++) {
     let dataCollector = dataCollectors[i];
-    if (!dataCollector.inTraining) {
-        let caseReport = new CaseReportForListing();
+    if (dataCollector.inTraining) {
+        let caseReport = new TrainingCaseReportForListing();
         caseReport.id = Guid.create();
         caseReport.message = '1#1#1';
         caseReport.origin = '123456789';
@@ -25,17 +25,17 @@ for(let i = 0; i < 5; i++) {
         caseReport.timestamp = new Date();
         caseReport.location = dataCollector.location;
         caseReport.dataCollectorId = dataCollector.id;
-        caseReports.push(caseReport);
+        trainingCaseReports.push(caseReport);
     }
 }
 
 //for status = 1
 for(let i = 5; i < 10; i++) {
     let dataCollector = dataCollectors[i];
-    if (!dataCollector.inTraining) {
-        let caseReport = new CaseReportForListing();
+    if (dataCollector.inTraining) {
+        let caseReport = new TrainingCaseReportForListing();
         caseReport.id = Guid.create();
-        caseReport.message = 'Some bad message';
+        caseReport.message = 'Some bad training message';
         caseReport.origin = '123456789';
         caseReport.status = 1;
         caseReport.dataCollectorDisplayName = dataCollector.displayName;
@@ -52,8 +52,8 @@ for(let i = 5; i < 10; i++) {
         caseReport.timestamp = new Date();
         caseReport.location = dataCollector.location;
         caseReport.dataCollectorId = dataCollector.id;
-        caseReports.push(caseReport);
+        trainingCaseReports.push(caseReport);
     }
 }
 
-export default caseReports;
+export default trainingCaseReports;
