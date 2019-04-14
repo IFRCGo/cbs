1a5f0f159ab90590b11ae8361194f80f49990264
Sindre Wilting <sindrewilting@gmail.com>
2019-04-14 08:27:07 +0200

    Better selection of language

--- Source/Reporting/Web/src/app/app.component.ts
+++ Source/Reporting/Web/src/app/app.component.ts
@@ -9,35 +9,45 @@ export class AppComponent implements OnInit {
   title = 'cbs';
   selected_lang: any;
   language = [
-    { text: 'English', value: 'en' },
-    { text: 'French', value: 'fr'}
+    { text: 'English', value: 'en', selected: false },
+    { text: 'French', value: 'fr', selected: false}
   ]
   constructor(private translate: TranslateService) {}
 
     ngOnInit() {
-    this.initialLanguage();
+      this.initialLanguage();
     }
 
     changeLang(e) {
       const lang_stored = e.target.value;
-      console.log(lang_stored);
       if (lang_stored !== '') {
+        this.setSelectedLanguage(lang_stored);
         localStorage.setItem('lang', lang_stored);
         this.translate.use(lang_stored);
       }
     }
 
     initialLanguage() {
-      const lang = this.translate.getBrowserLang();
-      this.translate.setDefaultLang(lang);
-      console.log(lang);
-
+      let lang;
       const lang_stored = localStorage.getItem('lang');
-      if (lang_stored) {
-        console.log(lang_stored);
-        this.translate.use(lang_stored);
-        this.selected_lang = lang_stored;
+      if (lang_stored && this.language.map(_ => _.value).includes(lang_stored)) {
+        lang = lang_stored;
       }
+      else {
+        let browser_lang = this.translate.getBrowserLang();
+        if (browser_lang === '' || !this.language.map(_ => _.value).includes(browser_lang)) browser_lang = 'en';
+        lang = browser_lang;
+      }
+      this.setSelectedLanguage(lang);
+      this.translate.setDefaultLang(lang);
+      localStorage.setItem('lang', lang);
+      this.translate.use(lang_stored);
+      this.selected_lang = lang_stored;
+      
+    }
+
+    setSelectedLanguage(lang) {
+      this.language[this.language.findIndex(_ => _.value === lang)].selected = true;
     }
 
 }
