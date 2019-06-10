cdd37f349b8216bb78d9b27bd4f2e82bba6c47f7
gardnk <gard.kroyer@gmail.com>
2019-06-10 11:47:45 +0200

    excel exporter date and time separated #1076

--- Source/Reporting/Core/CaseReports/Formats/ExcelExporter.cs
+++ Source/Reporting/Core/CaseReports/Formats/ExcelExporter.cs
@@ -48,69 +48,71 @@ namespace Core.CaseReports.Formats
 
         void WriteRow(ExcelWorksheet worksheet, int row, CaseReportForListing report)
         {
-            worksheet.Cells[row, 1].Value = report.Timestamp.DateTime;
-            worksheet.Cells[row, 18].Value = report.Message;
+            worksheet.Cells[row, 1].Value = report.Timestamp.DateTime.ToString("dd/MM/yyyy");
+            worksheet.Cells[row, 2].Value = report.Timestamp.DateTime.ToString("HH:mm");
+            worksheet.Cells[row, 19].Value = report.Message;
 
             if (report.DataCollectorId != DataCollectorId.NotSet)
             {
-                worksheet.Cells[row, 3].Value = report.DataCollectorDisplayName;
-                worksheet.Cells[row, 4].Value = report.DataCollectorRegion;
-                worksheet.Cells[row, 5].Value = report.DataCollectorDistrict;
-                worksheet.Cells[row, 6].Value = report.DataCollectorVillage;
+                worksheet.Cells[row, 4].Value = report.DataCollectorDisplayName;
+                worksheet.Cells[row, 5].Value = report.DataCollectorRegion;
+                worksheet.Cells[row, 6].Value = report.DataCollectorDistrict;
+                worksheet.Cells[row, 7].Value = report.DataCollectorVillage;
             }
             else
             {
-                worksheet.Cells[row, 3].Value = "Origin: " + report.Origin;
+                worksheet.Cells[row, 4].Value = "Origin: " + report.Origin;
             }
 
             if (report.HealthRiskId != HealthRiskId.NotSet)
             {
-                worksheet.Cells[row, 2].Value = "Success";
-                worksheet.Cells[row, 7].Value = report.HealthRisk;
-                worksheet.Cells[row, 8].Value = report.NumberOfMalesUnder5;
-                worksheet.Cells[row, 9].Value = report.NumberOfMalesAged5AndOlder;
-                worksheet.Cells[row, 10].Value = report.NumberOfFemalesUnder5;
-                worksheet.Cells[row, 11].Value = report.NumberOfFemalesAged5AndOlder;
+                worksheet.Cells[row, 3].Value = "Success";
+                worksheet.Cells[row, 8].Value = report.HealthRisk;
+                worksheet.Cells[row, 9].Value = report.NumberOfMalesUnder5;
+                worksheet.Cells[row, 10].Value = report.NumberOfMalesAged5AndOlder;
+                worksheet.Cells[row, 11].Value = report.NumberOfFemalesUnder5;
+                worksheet.Cells[row, 12].Value = report.NumberOfFemalesAged5AndOlder;
                 
-                worksheet.Cells[row, 12].Value = report.NumberOfMalesUnder5 + report.NumberOfFemalesUnder5;
-                worksheet.Cells[row, 13].Value = report.NumberOfMalesAged5AndOlder + report.NumberOfFemalesAged5AndOlder;
-                worksheet.Cells[row, 14].Value = report.NumberOfMalesUnder5 + report.NumberOfMalesAged5AndOlder;
-                worksheet.Cells[row, 15].Value = report.NumberOfFemalesUnder5 + report.NumberOfFemalesAged5AndOlder;
-                worksheet.Cells[row, 16].Value = report.NumberOfMalesUnder5 + report.NumberOfMalesAged5AndOlder + report.NumberOfFemalesUnder5 + report.NumberOfFemalesAged5AndOlder;
+                worksheet.Cells[row, 13].Value = report.NumberOfMalesUnder5 + report.NumberOfFemalesUnder5;
+                worksheet.Cells[row, 14].Value = report.NumberOfMalesAged5AndOlder + report.NumberOfFemalesAged5AndOlder;
+                worksheet.Cells[row, 15].Value = report.NumberOfMalesUnder5 + report.NumberOfMalesAged5AndOlder;
+                worksheet.Cells[row, 16].Value = report.NumberOfFemalesUnder5 + report.NumberOfFemalesAged5AndOlder;
+                worksheet.Cells[row, 17].Value = report.NumberOfMalesUnder5 + report.NumberOfMalesAged5AndOlder + report.NumberOfFemalesUnder5 + report.NumberOfFemalesAged5AndOlder;
             }
             else
             {
-                worksheet.Cells[row, 2].Value = "Error";
-                worksheet.Cells[row, 19].Value = string.Join(", ", report.ParsingErrorMessage);
+                worksheet.Cells[row, 3].Value = "Error";
+                worksheet.Cells[row, 20].Value = string.Join(", ", report.ParsingErrorMessage);
             }
 
             if (report.Location != Location.NotSet)
             {
-                worksheet.Cells[row, 17].Value = $"{report.Location.Latitude.ToString("F4")}/{report.Location.Longitude.ToString("F4")}";
+                worksheet.Cells[row, 18].Value = $"{report.Location.Latitude.ToString("F4")}/{report.Location.Longitude.ToString("F4")}";
             }
         }
 
         void WriteHeaders(ExcelWorksheet worksheet)
         {
-            worksheet.Cells["A1"].Value = "Time";               // 1
-            worksheet.Cells["B1"].Value = "Status";             // 2
-            worksheet.Cells["C1"].Value = "Datacollector";      // 3
-            worksheet.Cells["D1"].Value = "Region";             // 4
-            worksheet.Cells["E1"].Value = "District";           // 5
-            worksheet.Cells["F1"].Value = "Village";            // 6
-            worksheet.Cells["G1"].Value = "Health risk";        // 7
-            worksheet.Cells["H1"].Value = "Males under 5";      // 8
-            worksheet.Cells["I1"].Value = "Males 5 or older";   // 9
-            worksheet.Cells["J1"].Value = "Females under 5";    // 10
-            worksheet.Cells["K1"].Value = "Females 5 or older"; // 11
-            worksheet.Cells["L1"].Value = "Total under 5";      // 12
-            worksheet.Cells["M1"].Value = "Total 5 or older";   // 13
-            worksheet.Cells["N1"].Value = "Total females";      // 14
-            worksheet.Cells["O1"].Value = "Total males";        // 15
-            worksheet.Cells["P1"].Value = "Total people";       // 16
-            worksheet.Cells["Q1"].Value = "Location";           // 17
-            worksheet.Cells["R1"].Value = "Message";            // 18
-            worksheet.Cells["S1"].Value = "Errors";             // 19
+            worksheet.Cells["A1"].Value = "Date";               // 1
+            worksheet.Cells["B1"].Value = "Time";               // 2
+            worksheet.Cells["C1"].Value = "Status";             // 3
+            worksheet.Cells["D1"].Value = "Datacollector";      // 4
+            worksheet.Cells["E1"].Value = "Region";             // 5
+            worksheet.Cells["F1"].Value = "District";           // 6
+            worksheet.Cells["G1"].Value = "Village";            // 7
+            worksheet.Cells["H1"].Value = "Health risk";        // 8
+            worksheet.Cells["I1"].Value = "Males under 5";      // 9
+            worksheet.Cells["J1"].Value = "Males 5 or older";   // 10
+            worksheet.Cells["K1"].Value = "Females under 5";    // 11
+            worksheet.Cells["L1"].Value = "Females 5 or older"; // 12
+            worksheet.Cells["M1"].Value = "Total under 5";      // 13
+            worksheet.Cells["N1"].Value = "Total 5 or older";   // 14
+            worksheet.Cells["O1"].Value = "Total females";      // 15
+            worksheet.Cells["P1"].Value = "Total males";        // 16
+            worksheet.Cells["Q1"].Value = "Total people";       // 17
+            worksheet.Cells["R1"].Value = "Location";           // 18
+            worksheet.Cells["S1"].Value = "Message";            // 19
+            worksheet.Cells["T1"].Value = "Errors";             // 20
 
             worksheet.Row(1).Style.Font.Bold = true;
             worksheet.Cells["A1:P1"].AutoFilter = true;
