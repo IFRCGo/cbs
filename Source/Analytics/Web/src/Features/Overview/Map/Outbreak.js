9f639a3c9b29a29c0ff86343c5b47d3aa99a9096
Oscar <94oscarjohansson@gmail.com>
2019-06-30 19:34:32 +0200

    Added a handler for the map frontend that will give the data for the map in Analytics

---
+++ Source/Analytics/Web/src/Features/Overview/Map/Outbreak.js
@@ -0,0 +1,18 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated ReadModel Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { ReadModel } from  '@dolittle/readmodels';
+
+export class Outbreak extends ReadModel
+{
+    constructor() {
+        super();
+        this.artifact = {
+           id: 'a87cfaf1-54a9-4bfa-b291-03e746323643',
+           generation: '1'
+        };
+        this.id = 0;
+        this.outbreaks = [];
+    }
+}
---
+++ Source/Analytics/Web/src/Features/Overview/Map/OutbreakQuery.js
@@ -0,0 +1,15 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  This file is an automatically generated Query Proxy
+ *  
+ *--------------------------------------------------------------------------------------------*/
+import { Query } from  '@dolittle/queries';
+
+export class OutbreakQuery extends Query
+{
+    constructor() {
+        super();
+        this.nameOfQuery = 'OutbreakQuery';
+        this.generatedFrom = 'Read.Overview.Map.OutbreakQuery';
+
+    }
+}
