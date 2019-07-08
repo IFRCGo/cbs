8f2a737c3bcea2b3a011d327f10baa4904126a58
Oscar <94oscarjohansson@gmail.com>
2019-07-08 10:23:30 +0200

    Minor changes

---
+++ Source/Analytics/Core/Overview/Outbreak/OutbreakController.cs
@@ -0,0 +1,42 @@
+using System.Security.Claims;
+using Concepts;
+using Dolittle.AspNetCore.Execution;
+using Dolittle.DependencyInversion;
+using Dolittle.Execution;
+using Dolittle.ReadModels;
+using Dolittle.Security;
+using Dolittle.Tenancy;
+using Microsoft.AspNetCore.Mvc;
+using Read.Overview.Map;
+using Read.Overview.LastWeekTotals;
+using System;
+
+namespace Core.Overview.OutbreakController
+{
+    [Route("api/CaseReport/Outbreak")]
+    public class OutbreakController : ControllerBase
+    {
+        readonly IExecutionContextConfigurator _executionContextConfigurator;
+        readonly ITenantResolver _tenantResolver;
+        readonly FactoryFor<IReadModelRepositoryFor<Outbreak>> _Outbreaks;
+
+        public OutbreakController(
+            IExecutionContextConfigurator executionContextConfigurator,
+            ITenantResolver tenantResolver,
+            FactoryFor<IReadModelRepositoryFor<Outbreak>> Outbreaks)
+        {
+            _executionContextConfigurator = executionContextConfigurator;
+            _tenantResolver = tenantResolver;
+            _Outbreaks = Outbreaks;
+        }
+
+
+        [HttpGet("Outbreaks")]
+        public IActionResult Outbreaks() 
+        {
+            _executionContextConfigurator.ConfigureFor(_tenantResolver.Resolve(HttpContext.Request), CorrelationId.New(), ClaimsPrincipal.Current.ToClaims());
+            
+            return Ok(_Outbreaks().GetById(Day.Today)); 
+        }
+    }
+}
