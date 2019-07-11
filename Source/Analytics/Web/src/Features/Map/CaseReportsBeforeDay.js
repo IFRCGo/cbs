e77dadbb92a7ad6a79f7fcc37f062be609f9ef7b
Oscar <94oscarjohansson@gmail.com>
2019-07-11 14:36:57 +0200

    Added working map with clustering

--- Source/Analytics/Web/src/Features/Map/CaseReportsBeforeDay.js
+++ Source/Analytics/Web/src/Features/Map/CaseReportsBeforeDay.js
@@ -12,5 +12,7 @@ export class CaseReportsBeforeDay extends ReadModel
            id: '7bd7e2b4-8be2-4f73-bd2a-4a5a02ee67a2',
            generation: '1'
         };
+        this.id = 0;
+        this.casesPerHealthRisk = [];
     }
 }
