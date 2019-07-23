4ada72d43ee666e8c38fac345e42ebf03b59ed11
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-23 14:33:17 +0200

    Set up babel transpiler

--- Source/Analytics/Web/src/components/CountryOverview/CountryOverview.js
+++ Source/Analytics/Web/src/components/CountryOverview/CountryOverview.js
@@ -6,7 +6,7 @@ import CaseReportByHealthRiskTable from "../healthRisk/CaseReportByHealthRiskTab
 import HealthRiskPerDistrictTable from "../healthRisk/HealthRiskPerDistrictTable";
 import LastWeekTotals from '../LastWeekTotals.js';
 import Map from "../Map.js";
-import CBSNavigation from '../../../../../Navigation/Web.React/CBSNavigation';
+import CBSNavigation from '../../../../../Navigation/Web.React/lib/index.js';
 
 const appInsights = new ApplicationInsights({
     config: {
---
+++ Source/Navigation/Web.React/readme.md
@@ -0,0 +1,6 @@
+To run:
+1. In this folder, run `npm link`
+2. In the web folder you want to use, run `npm link ../../Web.React` so that you navigate back to this folder
+
+To update: 
+Run `babel index.js lib/index.js`
