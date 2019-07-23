483b6f065e29f579f54174ff6a6cedd85c0da63d
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-23 09:47:15 +0200

    Create table for age and sex distribution

--- Source/Analytics/Web/src/components/LastWeekTotals.js
+++ Source/Analytics/Web/src/components/LastWeekTotals.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-import Grid from '@material-ui/core/Grid';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
 
 import TotalCard from "./TotalCard";
 
@@ -8,15 +15,16 @@ import { CaseReportTotalsQuery } from "../Features/Overview/LastWeekTotals/CaseR
 import { QueryCoordinator } from "@dolittle/queries";
 
 
+
 export default class LastWeekTotals extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            totalFemale: "-",
-            totalMale: "-",
-            totalUnder5: "-",
-            totalOver5: "-",
+            totalFemale: "5",
+            totalMale: "6",
+            totalUnder5: "7",
+            totalOver5: "8",
             isLoading: true,
             isError: false
         };
@@ -51,9 +59,54 @@ export default class LastWeekTotals extends Component {
         this.fetchLastWeekTotals();
     }
 
+    /*
+    Table
+    TableBody
+    TableCell
+    TableHead
+    TableRow
+    Typography
+    Paper
+    */
     render () {
+        var femalesUnder5 = parseInt(this.state.totalFemale) + parseInt(this.state.totalMale) - parseInt(this.state.totalOver5); //wrong logic!
+        var malesUnder5 = parseInt(this.state.totalFemale) + parseInt(this.state.totalMale) - parseInt(this.state.totalOver5);
+
         return (
-            <>
+            <Paper>
+                <Table>
+                    <TableHead>Reports per sex and age (last 7 days)</TableHead>
+                    <TableRow>
+                        <TableCell>Age</TableCell>
+                        <TableCell>Female</TableCell>
+                        <TableCell>Male</TableCell>
+                        <TableCell>Total</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>Under 5</TableCell>
+                        <TableCell>{femalesUnder5}</TableCell>
+                        <TableCell>{malesUnder5}</TableCell>
+                        <TableCell>{this.state.totalUnder5}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>5 and above</TableCell>
+                        <TableCell>{femalesUnder5}</TableCell>
+                        <TableCell>{malesUnder5}</TableCell>
+                        <TableCell>{this.state.totalUnder5}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>Total</TableCell>
+                        <TableCell>{this.state.totalFemale}</TableCell>
+                        <TableCell>{this.state.totalMale}</TableCell>
+                        <TableCell>{parseInt(this.state.totalFemale)+parseInt(this.state.totalMale)}</TableCell>
+                    </TableRow>
+                </Table>
+            </Paper>
+        );
+    }
+}
+/*
+<>
                 <Grid item xs={3} key="TotalSex" style={{ height: 'auto' }}>
                     <Typography component="h2" variant="headline">
                         No. of cases by gender
@@ -69,6 +122,5 @@ export default class LastWeekTotals extends Component {
                     <TotalCard className={"fa fa-user"} subTitle={"Over 5"} total={this.state.totalOver5}  />
                 </Grid>
             </>
-        );
-    }
-}
+
+            */
