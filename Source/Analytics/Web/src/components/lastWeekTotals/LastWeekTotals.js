7a80e6351668c383615dfded5e1345091a4fac6b
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-23 10:53:33 +0200

    Style to equal sketch

--- Source/Analytics/Web/src/components/lastWeekTotals/LastWeekTotals.js
+++ Source/Analytics/Web/src/components/lastWeekTotals/LastWeekTotals.js
@@ -61,45 +61,40 @@ export default class LastWeekTotals extends Component {
         var totalOver5 = parseInt(this.state.femalesOver5) + parseInt(this.state.malesOver5);
         var totalFemale = parseInt(this.state.femalesUnder5) + parseInt(this.state.femalesOver5);
         var totalMale = parseInt(this.state.malesUnder5) + parseInt(this.state.malesOver5);
-        
-        var tableStyle = {
-            fontFamily: "Lato"
-        }
+
         return (
             <div class="tableContainer">
-                <h1 className="headline">Reports per sex and age (last 7 days)</h1>
-                <Paper>
-                    <Table style={tableStyle}>
-                        <TableHead className="tableHead">
-                            <TableRow>
-                                <TableCell className="headerText">Age</TableCell>
-                                <TableCell className="headerText">Female <i class="fa fa-venus"></i></TableCell>
-                                <TableCell className="headerText">Male <i class="fa fa-mars"></i></TableCell>
-                                <TableCell className="headerText">Total</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            <TableRow>
-                                <TableCell>Under 5</TableCell>
-                                <TableCell>{this.state.femalesUnder5}</TableCell>
-                                <TableCell>{this.state.malesUnder5}</TableCell>
-                                <TableCell>{totalUnder5}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>5 and above</TableCell>
-                                <TableCell>{this.state.femalesOver5}</TableCell>
-                                <TableCell>{this.state.malesOver5}</TableCell>
-                                <TableCell>{totalOver5}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell>Total</TableCell>
-                                <TableCell>{totalFemale}</TableCell>
-                                <TableCell>{totalMale}</TableCell>
-                                <TableCell>{totalFemale + totalMale}</TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </Paper>
+            <h2 className="headline">Reports per sex and age (last 7 days)</h2>
+                <Table className="table">
+                    <TableHead className="tableHead">
+                        <TableRow>
+                            <TableCell className="headerText">Age</TableCell>
+                            <TableCell className="headerText"> <i class="fa fa-venus"> </i> Female</TableCell>
+                            <TableCell className="headerText"> <i class="fa fa-mars"></i> Male</TableCell>
+                            <TableCell className="headerText">Total</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>Under 5</TableCell>
+                            <TableCell>{this.state.femalesUnder5}</TableCell>
+                            <TableCell>{this.state.malesUnder5}</TableCell>
+                            <TableCell>{totalUnder5}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>5 and above</TableCell>
+                            <TableCell>{this.state.femalesOver5}</TableCell>
+                            <TableCell>{this.state.malesOver5}</TableCell>
+                            <TableCell>{totalOver5}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>Total</TableCell>
+                            <TableCell>{totalFemale}</TableCell>
+                            <TableCell>{totalMale}</TableCell>
+                            <TableCell>{totalFemale + totalMale}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
             </div>
         );
     }
