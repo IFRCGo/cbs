94b14b0e8a32ec4c6441730a36aa9fb088da094b
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-24 09:47:17 +0200

    Render CSS

--- Source/Reporting/Web/src/app/navigation/navbar-host.component.ts
+++ Source/Reporting/Web/src/app/navigation/navbar-host.component.ts
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {Component, ViewChild,ElementRef,AfterContentInit} from '@angular/core';
+import {Component, ViewChild,ElementRef,AfterContentInit, ViewEncapsulation} from '@angular/core';
 import CBSNavigation from './test';
 
 @Component({
   selector: 'navbar-host',
   templateUrl:'./navbar-host.component.html',
-  styleUrls: ['./navbar-host.component.scss']
+  styleUrls: ['./navbar-host.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 
 export class NavbarHostComponent implements AfterContentInit{
