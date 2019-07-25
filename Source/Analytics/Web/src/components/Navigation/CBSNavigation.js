36f4ed8508879e81021f567cd1e525fd08c3f04c
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-25 10:14:22 +0200

    Lots of styling fixes

--- Source/Analytics/Web/src/components/Navigation/CBSNavigation.js
+++ Source/Analytics/Web/src/components/Navigation/CBSNavigation.js
@@ -8,8 +8,7 @@ class CBSNavigation extends Component {
 
     constructor(props) {
         super(props);
-        this.onMouseOver = this.onMouseOver.bind(this);
-        this.onMouseLeave = this.onMouseLeave.bind(this);
+        this.onClick = this.onClick.bind(this);
 
         this.state = {
             username: "Unknown", 
@@ -17,14 +16,10 @@ class CBSNavigation extends Component {
         };
     }
 
-    onMouseOver() {
-        this.setState({showAnalyticsDropdown: true});
+    onClick() {
+        this.setState(prevState => ({showAnalyticsDropdown: !prevState.showAnalyticsDropdown}));
       }
     
-      onMouseLeave() {
-        this.setState({showAnalyticsDropdown: false});
-      }
-
     fetchData() {
         if (process.env.environment !== 'production') {
             this.url = `http://www.mocky.io/v2/5cdc46d52d00003b12f5a6da`;
@@ -69,15 +64,21 @@ class CBSNavigation extends Component {
         var mainColor = (color == "red" ? "#fff" : "#c00");
 
         return(
-            <figure className={`logo logo-${color}`}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="70" height="40" viewBox="0 0 175 100">
-                    <rect width="175" height="100" fill={mainColor} />
-                    <path d="M20,50h66m-33,-33v66" fill="none" stroke={backgroundColor} strokeWidth="20" />
-                    <circle cx="132" cy="50" r="34" fill={backgroundColor} />
-                    <circle cx="142" cy="50" r="28" fill={mainColor} />
-                    <path d="M7,7H168V93H7z" fill="none" stroke={backgroundColor} strokeWidth="3" />
-                </svg>
-            </figure>
+            <div className={`logo logo-${color}`}>
+                <figure>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="70" height="40" viewBox="0 0 175 100">
+                        <rect width="175" height="100" fill={mainColor} />
+                        <path d="M20,50h66m-33,-33v66" fill="none" stroke={backgroundColor} strokeWidth="20" />
+                        <circle cx="132" cy="50" r="34" fill={backgroundColor} />
+                        <circle cx="142" cy="50" r="28" fill={mainColor} />
+                        <path d="M7,7H168V93H7z" fill="none" stroke={backgroundColor} strokeWidth="3" />
+                    </svg>
+                </figure>
+                <div className="logo-text">
+                    CBS
+                </div>
+            </div>
+
         )
     }
 
@@ -85,10 +86,10 @@ class CBSNavigation extends Component {
         return (
             <div>
                 <div className="dropdown-content">
-                    <a href="/analytics/" className="dropdown-item">Health risks</a>
-                    <a href="/analytics/" className="dropdown-item">Regions</a>
-                    <a href="/analytics/" className="dropdown-item">Light area overview</a>
-                    <a href="/analytics/" className="dropdown-item">Volunteer performance</a>
+                    <a href="/analytics/" className="dropdown-item"><div className="d-i-text">Health risks</div></a>
+                    <a href="/analytics/" className="dropdown-item"><div className="d-i-text">Regions</div></a>
+                    <a href="/analytics/" className="dropdown-item"><div className="d-i-text">Light area overview</div></a>
+                    <a href="/analytics/" className="dropdown-item"><div className="d-i-text">Volunteer performance</div></a>
                 </div>
             </div>
         )
@@ -98,15 +99,13 @@ class CBSNavigation extends Component {
         var active = this.props.activeMenuItem;
         
         return <a 
-                    href={`/{url}/`} 
+                    href={`/${url}/`} 
                     className={`menu-item ${url == active ? `active` : ``}`}>
-                        {text} {url=="analytics-new" &&  <i className="fa fa-caret-down"/>}
+                    {text} {url=="analytics/#" && <i className={`fa ${this.state.showAnalyticsDropdown ? `fa-chevron-up` : `fa-chevron-down`}`}/>}
                 </a>
-        
     }
 
     render() {
-        
         return (
             <header className="navigation-header">
                 {this.rcLogo("white")}
@@ -114,13 +113,11 @@ class CBSNavigation extends Component {
                 <nav>
                     {this.createMenuItem("analytics", "Country Overview")}
 
-                    <div className="dropdown" onMouseOver={this.onMouseOver} onMouseLeave={this.onMouseLeave}>
-
-                        {this.createMenuItem("analytics-new", `Analytics`)}
-
+                    <div className="dropdown" onClick={this.onClick}>
+                        {this.createMenuItem("analytics/#", `Analytics`)}
                         {this.state.showAnalyticsDropdown && this.analyticsDropdown()}
-
                     </div>
+
                     {this.createMenuItem("admin", "Project Administration")}
                     {this.createMenuItem("reporting/datacollectors", "Data Collectors")}
                     {this.createMenuItem("reporting/case-reports", "Reports")}
