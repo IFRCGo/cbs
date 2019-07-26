738ad82ba8745cb390c055f641f012408cd1442d
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-26 10:40:33 +0200

    Add totals

--- Source/Analytics/Web/src/components/healthRisk/CaseReportByHealthRiskTable.js
+++ Source/Analytics/Web/src/components/healthRisk/CaseReportByHealthRiskTable.js
@@ -53,32 +53,51 @@ class CaseReportByHealthRiskTable extends Component {
                 <TableHead className="tableHead">
                   <TableRow>
                     <TableCell></TableCell>
-                    <TableCell className="headerText">0-6 days</TableCell>
-                    <TableCell className="headerText">7-13 days</TableCell>
-                    <TableCell className="headerText">14-21 days</TableCell>
-                    <TableCell className="headerText">22-27 days</TableCell>
+                    <TableCell className="headerText">Reports last week (today - day 6)</TableCell>
+                    <TableCell className="headerText">Reports 1 week ago (day 7-13)</TableCell>
+                    <TableCell className="headerText">Reports 2 weeks ago (day 14-20)</TableCell>
+                    <TableCell className="headerText">Reports 3 weeks ago (day 21-27)</TableCell>
+                    <TableCell className="headerText">Total no. of reports (last 28 days)</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {Object.keys(this.state.healthRisks).map(key => (
+                  {Object.keys(this.state.healthRisks).map(key => {
+                  var total = this.state.healthRisks[key].days0to6
+                              + this.state.healthRisks[key].days7to13
+                              + this.state.healthRisks[key].days14to20
+                              + this.state.healthRisks[key].days21to27;
+                  return (
                     <TableRow key={this.state.healthRisks[key].healthRiskName}>
                       <TableCell className="cell">
                         {this.state.healthRisks[key].healthRiskName}
                       </TableCell>
-                      <TableCell className="cell" style={this.state.healthRisks[key].days0to6 === 0 ? {color: "#B5B5B5"} : {}}>
+                      <TableCell 
+                        className="cell"
+                        style={this.state.healthRisks[key].days0to6 === 0 ? {color: "#B5B5B5"} : {}}>
                         {this.state.healthRisks[key].days0to6}
                       </TableCell>
-                      <TableCell className="cell" style={this.state.healthRisks[key].days7to13 === 0 ? {color: "#B5B5B5"} : {}}>
+                      <TableCell 
+                        className="cell" 
+                        style={this.state.healthRisks[key].days7to13 === 0 ? {color: "#B5B5B5"} : {}}>
                         {this.state.healthRisks[key].days7to13}
                       </TableCell>
-                      <TableCell className="cell" style={this.state.healthRisks[key].days14to20 === 0 ? {color: "#B5B5B5"} : {}}>
+                      <TableCell 
+                        className="cell" 
+                        style={this.state.healthRisks[key].days14to20 === 0 ? {color: "#B5B5B5"} : {}}>
                         {this.state.healthRisks[key].days14to20}
                       </TableCell>
-                      <TableCell className="cell" style={this.state.healthRisks[key].days21to27 === 0 ? {color: "#B5B5B5"} : {}}>
+                      <TableCell 
+                        className="cell" 
+                        style={this.state.healthRisks[key].days21to27 === 0 ? {color: "#B5B5B5"} : {}}>
                         {this.state.healthRisks[key].days21to27}
                       </TableCell>
+                      <TableCell 
+                        className="cell" 
+                        style={total === 0 ? {color: "#B5B5B5"} : {}}>
+                        {total}
+                      </TableCell>
                     </TableRow>
-                  ))}
+                  )})}
                 </TableBody>
               </Table>
           </div>
