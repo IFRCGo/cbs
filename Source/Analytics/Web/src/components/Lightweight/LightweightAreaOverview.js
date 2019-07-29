1586c9c40808ff72e801b804e4e1030f2591b048
gardnk <gard.kroyer@gmail.com>
2019-07-29 13:20:31 +0200

    added menu to lightweight overview

--- Source/Analytics/Web/src/components/Lightweight/LightweightAreaOverview.js
+++ Source/Analytics/Web/src/components/Lightweight/LightweightAreaOverview.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import HealthRiskSelector from '../healthRisk/HealthRiskSelector';
+import CBSNavigation from '../Navigation/CBSNavigation';
 import './LightweightAreaOverview.scss';
 
 export default class LightweightAreaOverview extends Component {
@@ -11,6 +12,8 @@ export default class LightweightAreaOverview extends Component {
     render() {
         return (
             <div>
+                <CBSNavigation activeMenuItem="analytics/" />
+
                 <h2 className="lightweight header">Light Area Overview</h2>
                 <p className="lightweight description">This is the light version of the country overview page. If you want the normal version click <Link to="/analytics">here</Link></p>
                 <HealthRiskSelector />
