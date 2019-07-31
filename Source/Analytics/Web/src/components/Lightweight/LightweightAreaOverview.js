0bb9875799c82db5c5c786ada55a8f1e0f18efa4
Agnete Djupvik <agnetedjupvik@gmail.com>
2019-07-31 11:22:42 +0200

    Implement grid layout on lightweight front page

--- Source/Analytics/Web/src/components/Lightweight/LightweightAreaOverview.js
+++ Source/Analytics/Web/src/components/Lightweight/LightweightAreaOverview.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
 import HealthRiskSelector from '../healthRisk/HealthRiskSelector';
 import CBSNavigation from '../Navigation/CBSNavigation';
-import './LightweightAreaOverview.scss';
+import '../../assets/main.scss';
 
 export default class LightweightAreaOverview extends Component {
     constructor(props) {
@@ -11,14 +12,19 @@ export default class LightweightAreaOverview extends Component {
 
     render() {
         return (
-            <div>
-                <CBSNavigation activeMenuItem="analytics/#" />
-                <div className="lightweight">
-                    <h2 className="header">Light Area Overview</h2>
-                    <p className="description">This is the light version of the country overview page. If you want the normal version click <Link to="/analytics">here</Link></p>
-                </div>
-                <HealthRiskSelector />
-            </div>
+            <Grid container>
+                <Grid item xs={12}>
+                    <CBSNavigation activeMenuItem="analytics/#" />
+                </Grid>
+                
+                <Grid container item xs={12} justify="center">
+                    <div className="lightweight">
+                        <h1>Light Area Overview</h1>
+                        <p>This is the light version of the country overview page. If you want the normal version click <Link to="/analytics">here</Link></p>
+                    </div>
+                    <HealthRiskSelector />
+                </Grid>
+            </Grid>
         );
     }
 }
