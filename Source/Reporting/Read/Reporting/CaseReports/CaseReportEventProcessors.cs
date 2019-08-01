e684a45f9d661411996bf22ca9b41e6f63f30256
gardnk <gard.kroyer@gmail.com>
2019-08-01 16:08:29 +0200

    revert changes to CaseReportEventProcessor

--- Source/Reporting/Read/Reporting/CaseReports/CaseReportEventProcessors.cs
+++ Source/Reporting/Read/Reporting/CaseReports/CaseReportEventProcessors.cs
@@ -8,9 +8,7 @@ using Dolittle.Events.Processing;
 using Dolittle.Logging;
 using Dolittle.ReadModels;
 using Events.Reporting.CaseReports;
-using Events.NotificationGateway.Reporting.SMS;
 using Dolittle.Runtime.Events;
-using Read.Management.DataCollectors;
 
 namespace Read.Reporting.CaseReports
 {
@@ -19,60 +17,33 @@ namespace Read.Reporting.CaseReports
         readonly ILogger _logger;
         readonly IReadModelRepositoryFor<CaseReport> _caseReports;
         readonly IReadModelRepositoryFor<CaseReportFromUnknownDataCollector> _caseReportsFromUnknownDataCollectors;
-        readonly IReadModelRepositoryFor<TrainingReport> _trainingReports;
-        readonly IReadModelRepositoryFor<DataCollector> _dataCollectors;
 
         public CaseReportEventProcessor(
             ILogger logger,
             IReadModelRepositoryFor<CaseReport> caseReports,
-            IReadModelRepositoryFor<CaseReportFromUnknownDataCollector> caseReportsFromUnknownDataCollectors,
-            IReadModelRepositoryFor<TrainingReport> trainingReports,
-            IReadModelRepositoryFor<DataCollector> dataCollectors)
+            IReadModelRepositoryFor<CaseReportFromUnknownDataCollector> caseReportsFromUnknownDataCollectors)
         {
             _logger = logger;
             _caseReports = caseReports;
             _caseReportsFromUnknownDataCollectors = caseReportsFromUnknownDataCollectors;
-            _trainingReports = trainingReports;
-            _dataCollectors = dataCollectors;
         }
         [EventProcessor("7f3b6037-6b2f-448b-8f14-0735330a50e0")]
         public void Process(CaseReportReceived @event, EventSourceId caseReportId)
         {
-            var dataCollector = _dataCollectors.GetById(@event.DataCollectorId);
-            if (dataCollector.InTraining)
+            var caseReport = new CaseReport(caseReportId.Value)
             {
-                var caseReport = new TrainingReport(caseReportId.Value)
-                {
-                    DataCollectorId = @event.DataCollectorId,
-                    HealthRiskId = @event.HealthRiskId,
-                    NumberOfFemalesUnder5 = @event.NumberOfFemalesUnder5,
-                    NumberOfFemalesAged5AndOlder = @event.NumberOfFemalesAged5AndOlder,
-                    NumberOfMalesUnder5 = @event.NumberOfMalesUnder5,
-                    NumberOfMalesAged5AndOlder = @event.NumberOfMalesAged5AndOlder,
-                    Location = new Location(@event.Latitude, @event.Longitude),
-                    Timestamp = @event.Timestamp,
-                    Message = @event.Message
-                };
+                DataCollectorId = @event.DataCollectorId,
+                HealthRiskId = @event.HealthRiskId,
+                NumberOfFemalesUnder5 = @event.NumberOfFemalesUnder5,
+                NumberOfFemalesAged5AndOlder = @event.NumberOfFemalesAged5AndOlder,
+                NumberOfMalesUnder5 = @event.NumberOfMalesUnder5,
+                NumberOfMalesAged5AndOlder = @event.NumberOfMalesAged5AndOlder,
+                Location = new Location(@event.Latitude, @event.Longitude),
+                Timestamp = @event.Timestamp,
+                Message = @event.Message
+            };
 
-                _trainingReports.Insert(caseReport);
-            }
-            else
-            {
-                var caseReport = new CaseReport(caseReportId.Value)
-                {
-                    DataCollectorId = @event.DataCollectorId,
-                    HealthRiskId = @event.HealthRiskId,
-                    NumberOfFemalesUnder5 = @event.NumberOfFemalesUnder5,
-                    NumberOfFemalesAged5AndOlder = @event.NumberOfFemalesAged5AndOlder,
-                    NumberOfMalesUnder5 = @event.NumberOfMalesUnder5,
-                    NumberOfMalesAged5AndOlder = @event.NumberOfMalesAged5AndOlder,
-                    Location = new Location(@event.Latitude, @event.Longitude),
-                    Timestamp = @event.Timestamp,
-                    Message = @event.Message
-                };
-
-                _caseReports.Insert(caseReport);
-            }
+            _caseReports.Insert(caseReport);
         }
         [EventProcessor("980f8db1-2e3a-4609-b7e6-29cee5190ea8")]
         public void Process(CaseReportFromUnknownDataCollectorReceived @event, EventSourceId caseReportId)
