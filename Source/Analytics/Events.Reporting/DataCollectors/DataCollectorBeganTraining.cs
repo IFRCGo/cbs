902e7625bb8572f4227fa1968bd573c8d1df053c
gardnk <gard.kroyer@gmail.com>
2019-08-02 11:19:15 +0200

    added artifact ids to new events

--- Source/Analytics/Events.Reporting/DataCollectors/DataCollectorBeganTraining.cs
+++ Source/Analytics/Events.Reporting/DataCollectors/DataCollectorBeganTraining.cs
@@ -3,13 +3,14 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using Dolittle.Artifacts;
 using Dolittle.Events;
 
 namespace Events.Reporting.DataCollectors
 {
+    [Artifact("a73348f5-45ab-4913-8a6f-5947b44d6944")]
     public class DataCollectorBeganTraining : IEvent
     {
-
         public DataCollectorBeganTraining()
         {
         }
--- Source/Analytics/Events.Reporting/DataCollectors/DataCollectorCompletedTraining.cs
+++ Source/Analytics/Events.Reporting/DataCollectors/DataCollectorCompletedTraining.cs
@@ -3,10 +3,12 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using Dolittle.Artifacts;
 using Dolittle.Events;
 
 namespace Events.Reporting.DataCollectors
 {
+    [Artifact("950cef1b-5e9f-44e3-b410-67749573eda7")]
     public class DataCollectorCompletedTraining : IEvent
     {
         public DataCollectorCompletedTraining()
