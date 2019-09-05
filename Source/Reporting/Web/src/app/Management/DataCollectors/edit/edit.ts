374f6ba61ad093d5d296da3910b0f60fe1b6fbee
gardnk <gard.kroyer@gmail.com>
2019-09-05 13:29:59 +0200

    removed unused lifecycle hook #1191

--- Source/Reporting/Web/src/app/Management/DataCollectors/edit/edit.ts
+++ Source/Reporting/Web/src/app/Management/DataCollectors/edit/edit.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
