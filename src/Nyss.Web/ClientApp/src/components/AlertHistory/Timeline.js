7d1b57f8323a18fec5a18e10da3abdec3337803c
Alexandre Dos Santos <alex@hackages.io>
2019-10-18 15:11:15 +0200

    first test on List

--- src/Nyss.Web/ClientApp/src/components/AlertHistory/Timeline.js
+++ src/Nyss.Web/ClientApp/src/components/AlertHistory/Timeline.js
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 import { List } from './List'
 import { LocationDisplay } from './LocationDisplay'
-import FetchData from '../FetchData'
 
 export const Timeline = ({ data }) => {
   return (
     <div className='d-flex flex-row'>
       <LocationDisplay data={data} />
-      <List />
+      <List data={data} />
     </div>
   )
 }
