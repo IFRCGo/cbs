1b47443240d5bc0ad1e491461cc05922bc948445
Aylin <aylin@aylin.local>
2019-10-18 15:46:02 +0200

    front: fixed data display

--- src/Nyss.Web/ClientApp/src/components/AlertHistory/LocationDisplay.js
+++ src/Nyss.Web/ClientApp/src/components/AlertHistory/LocationDisplay.js
@@ -1,16 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 export class LocationDisplay extends Component {
   render() {
     const { data } = this.props
     return (
-      <Fragment>
+      <div>
         {data.map((item, index) => (
-          <span
-            key={index}
-          >{`${item.region} ${item.district} ${item.village}`}</span>
+          <div key={index}>
+            {`${item.region} ${item.district} ${item.village}`}
+          </div>
         ))}
-      </Fragment>
+      </div>
     )
   }
 }
