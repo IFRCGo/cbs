35bda93534d6bbd41bf6a253c4911f842fbaaede
Aylin <aylin@aylin.local>
2019-10-18 15:46:02 +0200

    front: fixed data display

--- src/Nyss.Web/ClientApp/src/components/AlertHistory/List.js
+++ src/Nyss.Web/ClientApp/src/components/AlertHistory/List.js
@@ -1,45 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 export class List extends Component {
   render() {
     const { data } = this.props
     console.log(data)
     return (
-      <div className="d-flex flex-column overflow-auto">
-        <div className="d-flex flex-row justify-content-between max-wdith ">
-          <div className="mx-2">val1</div>
-          <div className="mx-2">val2</div>
-          <div className="mx-2">val3</div>
-          <div className="mx-2">val4</div>
-          <div className="mx-2">val5</div>
-          <div className="mx-2">val6</div>
-          <div className="mx-2">val7</div>
-          <div className="mx-2">val8</div>
-          <div className="mx-2">val9</div>
-          <div className="mx-2">val10</div>
-          <div className="mx-2">val11</div>
+      <div className='d-flex flex-column overflow-auto'>
+        <div className='d-flex flex-row justify-content-between max-wdith '>
+          <div className='mx-2'>val1</div>
+          <div className='mx-2'>val2</div>
+          <div className='mx-2'>val3</div>
+          <div className='mx-2'>val4</div>
+          <div className='mx-2'>val5</div>
+          <div className='mx-2'>val6</div>
+          <div className='mx-2'>val7</div>
+          <div className='mx-2'>val8</div>
+          <div className='mx-2'>val9</div>
+          <div className='mx-2'>val10</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
+          <div className='mx-2'>val11</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
-        <div className="d-flex flex-row justify-content-between">
+        <div className='d-flex flex-row justify-content-between'>
           <div>azezaeze</div>
           <div>azeiozeo</div>
         </div>
