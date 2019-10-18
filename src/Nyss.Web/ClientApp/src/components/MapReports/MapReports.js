67d432349b55d669b8af28bd777084a9875f4e19
Philippe Haerens <haerphi@gmail.com>
2019-10-18 11:46:15 +0200

    some comment +style for cluster

--- src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
+++ src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
@@ -1,28 +1,34 @@
 import React, { useEffect, useRef } from "react";
 import L from "leaflet";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+//data import
 import {
   getCaseReports,
   getHealthRisks,
   getDataCollectors
 } from "./functions/fetchHealthData";
-import Modal from "./modal/modalReports";
+
+//mordal for each case
+// import Modal from "./modal/modalReports";
+
+//leaflet marker cluster group import
 import MarkerClusterGroup from "react-leaflet-markercluster";
+require("react-leaflet-markercluster/dist/styles.min.css");
 
 const MapReports = () => {
   console.log(getCaseReports());
   console.log(getHealthRisks());
   console.log(getDataCollectors());
-  //fonction de filter
+  //filter function
 
-  const createClusterCustomIcon = function (cluster) {
+  const createClusterCustomIcon = function(cluster) {
     return L.divIcon({
       html: `<span>${cluster.getChildCount()}</span>`,
-      className: 'marker-cluster-custom',
-      iconSize: L.point(40, 40, true),
+      className: "marker-cluster-custom",
+      iconSize: L.point(40, 40, true)
     });
   };
-
 
   return (
     <div className={"leaflet-map-container"}>
@@ -45,27 +51,24 @@ const MapReports = () => {
           url={"https://{s}.tile.osm.org/{z}/{x}/{y}.png"}
         />
 
-      {/* CLUSTER GROUP */}
-          <MarkerClusterGroup
-    showCoverageOnHover={false}
-    spiderfyDistanceMultiplier={2}
-    iconCreateFunction={createClusterCustomIcon}
-    
-  >
-
-    <Marker position={[49.8397, 24.0297]}/>
-    <Marker position={[50.4501, 30.5234]} />
-    <Marker position={[52.2297, 21.0122]} />
-    <Marker position={[50.0647, 19.9450]} />
-    <Marker position={[48.9226, 24.7111]} />
-    <Marker position={[48.7164, 21.2611]} />
-    <Marker position={[51.5, -0.09]}><Popup>{"plop"}</Popup></Marker>  
-    <Marker position={[51.5, -0.09]} />
-    <Marker position={[51.5, -0.09]} />
-
-  </MarkerClusterGroup>
-
-
+        {/* CLUSTER GROUP */}
+        <MarkerClusterGroup
+          showCoverageOnHover={false}
+          spiderfyDistanceMultiplier={2}
+          iconCreateFunction={createClusterCustomIcon}
+        >
+          <Marker position={[49.8397, 24.0297]} />
+          <Marker position={[50.4501, 30.5234]} />
+          <Marker position={[52.2297, 21.0122]} />
+          <Marker position={[50.0647, 19.945]} />
+          <Marker position={[48.9226, 24.7111]} />
+          <Marker position={[48.7164, 21.2611]} />
+          <Marker position={[51.5, -0.09]}>
+            <Popup>{"plop"}</Popup>
+          </Marker>
+          <Marker position={[51.5, -0.09]} />
+          <Marker position={[51.5, -0.09]} />
+        </MarkerClusterGroup>
 
         {/* <Marker position={[0, 0]}>
           <Popup>{`Hello world`}</Popup>
