136bb961731fd41e93de17cca0c20a2a4e3fcae1
Philippe Haerens <haerphi@gmail.com>
2019-10-18 12:14:44 +0200

    all maker in console.log
    need to get localisation by the collectorID

--- src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
+++ src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
@@ -29,6 +29,13 @@ const MapReports = () => {
       iconSize: L.point(40, 40, true)
     });
   };
+  const markers = () => {
+    return L.markerClusterGroup(
+      <Marker position={[0, 0]}>
+        <Popup>{`Hello world`}</Popup>
+      </Marker>
+    );
+  };
 
   return (
     <div className={"leaflet-map-container"}>
@@ -50,26 +57,24 @@ const MapReports = () => {
           }
           url={"https://{s}.tile.osm.org/{z}/{x}/{y}.png"}
         />
-
         {/* CLUSTER GROUP */}
         <MarkerClusterGroup
           showCoverageOnHover={false}
           spiderfyDistanceMultiplier={2}
           iconCreateFunction={createClusterCustomIcon}
         >
-          <Marker position={[49.8397, 24.0297]} />
-          <Marker position={[50.4501, 30.5234]} />
-          <Marker position={[52.2297, 21.0122]} />
-          <Marker position={[50.0647, 19.945]} />
-          <Marker position={[48.9226, 24.7111]} />
-          <Marker position={[48.7164, 21.2611]} />
-          <Marker position={[51.5, -0.09]}>
-            <Popup>{"plop"}</Popup>
-          </Marker>
-          <Marker position={[51.5, -0.09]} />
-          <Marker position={[51.5, -0.09]} />
+          {casesReports.map(el => {
+            console.log(el.DataCollectorId);
+            const lat = 0;
+            const long = 0;
+            return (
+              <Marker key={el._id} position={[lat, long]}>
+                <Popup>{`Hello world`}</Popup>
+              </Marker>
+            );
+          })}
         </MarkerClusterGroup>
-
+         
         {/* <Marker position={[0, 0]}>
           <Popup>{`Hello world`}</Popup>
         </Marker> */}
