81c64b7e81cd2e939e0c839db371eb4bf488c47e
Florence <florence.vermeer@gmail.com>
2019-10-18 11:23:21 +0200

    suff

--- src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
+++ src/Nyss.Web/ClientApp/src/components/MapReports/MapReports.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useRef } from "react";
-//import L from "leaflet";
+import L from "leaflet";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 
 const MapReports = () => {
+
+  const createClusterCustomIcon = function (cluster) {
+    return L.divIcon({
+      html: `<span>${cluster.getChildCount()}</span>`,
+      className: 'marker-cluster-custom',
+      iconSize: L.point(40, 40, true),
+    });
+  };
+
+
   return (
     <div className={"leaflet-map-container"}>
       <p>{"hello world"}</p>
@@ -24,9 +34,32 @@ const MapReports = () => {
           }
           url={"https://{s}.tile.osm.org/{z}/{x}/{y}.png"}
         />
-        <Marker position={[0, 0]}>
+
+      {/* CLUSTER GROUP */}
+          <MarkerClusterGroup
+    showCoverageOnHover={false}
+    spiderfyDistanceMultiplier={2}
+    iconCreateFunction={createClusterCustomIcon}
+    
+  >
+
+    <Marker position={[49.8397, 24.0297]}/>
+    <Marker position={[50.4501, 30.5234]} />
+    <Marker position={[52.2297, 21.0122]} />
+    <Marker position={[50.0647, 19.9450]} />
+    <Marker position={[48.9226, 24.7111]} />
+    <Marker position={[48.7164, 21.2611]} />
+    <Marker position={[51.5, -0.09]}><Popup>{"plop"}</Popup></Marker>  
+    <Marker position={[51.5, -0.09]} />
+    <Marker position={[51.5, -0.09]} />
+
+  </MarkerClusterGroup>
+
+
+
+        {/* <Marker position={[0, 0]}>
           <Popup>{`Hello world`}</Popup>
-        </Marker>
+        </Marker> */}
       </Map>
     </div>
   );
