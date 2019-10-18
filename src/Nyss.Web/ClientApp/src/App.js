2114ad9c71402a61aa69f8c585d0b4d1f7f7d5c9
Alexandre Dos Santos <alexandre@Alexandres-MacBook-Pro.local>
2019-10-18 11:38:22 +0200

    added route and Main container for Activity History
    Author:    Alexandre Dos Santos <alex@hackages.io>

--- src/Nyss.Web/ClientApp/src/App.js
+++ src/Nyss.Web/ClientApp/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
+import { Route, Switch } from 'react-router';
+
+import { MainContainer } from './components/AlertHistory/MainContainer';
 import Home from './components/Home';
+import Layout from './components/Layout';
 
 export default () => (
   <Layout>
-    <Route exact path='/' component={Home} />
+    <Switch>
+      <Route path="/activity-history" component={MainContainer} />
+      <Route exact path="/" component={Home} />
+    </Switch>
   </Layout>
-);
+)
--- src/Nyss.Web/ClientApp/src/components/Home.js
+++ src/Nyss.Web/ClientApp/src/components/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 const Home = props => (
   <div>
     <h1>Welcome to Nyss - the CBS platform</h1>
-    <p>First of all thank you for wanting to contribute!;
-    <br/>We hope you will have a great couple of days with us.</p>
-    <p>If you have any questions, please reach out to the people in blue (tech/ux) and red (domain) t-shirts!</p>
-    <a href='https://trello.com/invite/b/00j1CXLz/ec14ccda710559537037b4c7565b60c3/red-cross-codeathon-brussels-2019'>Join our trello board for an oveview of tasks</a>
-  </div>
-);
+    <p>
+      First of all thank you for wanting to contribute!;
+      <br />
+      We hope you will have a great couple of days with us.
+    </p>
+    <p>
+      If you have any questions, please reach out to the people in blue
+      (tech/ux) and red (domain) t-shirts!
+    </p>
 
-export default connect()(Home);
+    <a href="https://trello.com/invite/b/00j1CXLz/ec14ccda710559537037b4c7565b60c3/red-cross-codeathon-brussels-2019">
+      Join our trello board for an oveview of tasks
+    </a>
+  </div>
+)
+
+export default Home
