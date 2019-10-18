feee0c349810d3c0709ee9b3e12fb109db68c85d
Aylin <aylin@aylin.local>
2019-10-18 15:14:15 +0200

    front: added component mapping

--- src/Nyss.Web/ClientApp/src/components/AlertHistory/Timeline.js
+++ src/Nyss.Web/ClientApp/src/components/AlertHistory/Timeline.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 import { List } from './List'
 import { LocationDisplay } from './LocationDisplay'
+import FetchData from '../FetchData'
 
 export const Timeline = ({ data }) => {
   return (
-    <div class='d-flex flex-row'>
+    <div className='d-flex flex-row'>
       <LocationDisplay data={data} />
       <List />
     </div>
