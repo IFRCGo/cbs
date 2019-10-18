a29c73517b685d6206d69ca61a473999274503d8
Alexandre Dos Santos <alex@hackages.io>
2019-10-18 12:00:18 +0200

    front : mainContainer in a new folder

--- src/Nyss.Web/ClientApp/src/App.js
+++ src/Nyss.Web/ClientApp/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
-import { MainContainer } from './components/AlertHistory/MainContainer';
 import Home from './components/Home';
 import MapReports from './components/MapReports/MapReports';
+import Layout from './components/Layout';
+import { ActivityHistoryContainer } from './containers/ActivityHistoryContainer';
 
 export default () => (
   <Layout>
     <Switch>
-      <Route path="/activity-history" component={MainContainer} />
+      <Route path="/activity-history" component={ActivityHistoryContainer} />
       <Route exact path="/" component={Home} />
       <Route exact path='/map' component={MapReports} />
     </Switch>
