3dc8a5b39f0e2e3a37a01ce167e10f946d2c21cd
Kevin <kevin.tassi@gmail.com>
2019-10-19 12:23:55 +0200

    Expose endpoit to react router

--- src/Nyss.Web/ClientApp/src/App.js
+++ src/Nyss.Web/ClientApp/src/App.js
@@ -5,13 +5,15 @@ import Home from './components/Home';
 import Layout from './components/Layout';
 import { ActivityHistoryContainer } from './containers/ActivityHistoryContainer';
 import MapReports from './components/MapReports/MapReports';
+import AlertHistoryView from './views/AlertHistoryView'
 
 export default () => (
   <Layout>
     <Switch>
-      <Route path="/activity-history" component={ActivityHistoryContainer} />
-            <Route exact path="/" component={Home} />
-            <Route exact path='/map' component={MapReports} />
+      <Route exact path="/" component={ Home } />
+      <Route exact path='/map' component={ MapReports } />
+      <Route path="/activity-history" component={ ActivityHistoryContainer } />
+      <Route path="/alert-history" component={ AlertHistoryView } />
     </Switch>
   </Layout>
 )
