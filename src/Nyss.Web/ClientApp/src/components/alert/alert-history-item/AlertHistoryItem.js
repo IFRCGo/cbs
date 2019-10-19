513346f47fbf689ac5b5d73985319a30ce05b68d
Kevin <kevin.tassi@gmail.com>
2019-10-19 12:47:44 +0200

    Create alert history item component

---
+++ src/Nyss.Web/ClientApp/src/components/alert/alert-history-item/AlertHistoryItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AlertHistoryItem extends React.Component {
+  render() {
+    return (
+      <li>
+        item
+      </li>
+    )
+  }
+}
+
+export default AlertHistoryItem;
