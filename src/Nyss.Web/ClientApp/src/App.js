164c1507e410ff2ee297c73e4cb9cf422f07c3a4
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2019-10-19 13:52:27 +0200

    URL is caught in index.js and passed as props to App and ActiviyHistoryContainer

--- src/Nyss.Web/ClientApp/src/App.js
+++ src/Nyss.Web/ClientApp/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Route, Switch } from 'react-router';
+import React from 'react'
+import { Route, Switch } from 'react-router'
 
-import Home from './components/Home';
-import Layout from './components/Layout';
-import { ActivityHistoryContainer } from './containers/ActivityHistoryContainer';
-import MapReports from './components/MapReports/MapReports';
+import Home from './components/Home'
+import Layout from './components/Layout'
+import { ActivityHistoryContainer } from './containers/ActivityHistoryContainer'
+import MapReports from './components/MapReports/MapReports'
 
-export default () => (
+export default props => (
   <Layout>
     <Switch>
-      <Route path="/activity-history" component={ActivityHistoryContainer} />
-            <Route exact path="/" component={Home} />
-            <Route exact path='/map' component={MapReports} />
+      <Route
+        path='/activity-history'
+        url={props.url}
+        component={ActivityHistoryContainer}
+      />
+      <Route exact path='/' url={props.url} component={Home} />
+      <Route exact path='/map' url={props.url} component={MapReports} />
     </Switch>
   </Layout>
 )
