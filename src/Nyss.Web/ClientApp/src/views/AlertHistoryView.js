db24db2f0925049ea83db9c65b344c461b5a5ad9
Kevin <kevin.tassi@gmail.com>
2019-10-19 12:34:00 +0200

    Add alert history container

--- src/Nyss.Web/ClientApp/src/views/AlertHistoryView.js
+++ src/Nyss.Web/ClientApp/src/views/AlertHistoryView.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import AlertHistoryContainer from '../components/alert/alert-history-container/AlertHistoryContainer'
+
 class AlertHistoryView extends React.Component {
   render() {
     return (
       <div>
         <h1>Alert history:</h1>
+        <AlertHistoryContainer />
       </div>
     )
   }
