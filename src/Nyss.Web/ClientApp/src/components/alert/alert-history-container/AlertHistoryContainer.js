def2ee785048fd1d1c11361db88df7eb20cc9f8c
Kevin <kevin.tassi@gmail.com>
2019-10-19 13:43:10 +0200

    Add som style to item

--- src/Nyss.Web/ClientApp/src/components/alert/alert-history-container/AlertHistoryContainer.js
+++ src/Nyss.Web/ClientApp/src/components/alert/alert-history-container/AlertHistoryContainer.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
+import './AlertHistoryContainer.css'
 import AlertHistoryItem from '../alert-history-item/AlertHistoryItem'
 
 class AlertHistoryContainer extends React.Component {
   render() {
     return (
-      <div>
-        <ul>
-          <AlertHistoryItem />
-          <AlertHistoryItem />
-          <AlertHistoryItem />
-          <AlertHistoryItem />
+      <div className="alert-history-container">
+        <ul className="alert-container">
           <AlertHistoryItem />
         </ul>
       </div>
