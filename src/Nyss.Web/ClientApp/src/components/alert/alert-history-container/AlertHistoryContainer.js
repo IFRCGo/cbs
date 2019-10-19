06b9c6cbec41c7fa656895852e1224eba42477ee
Kevin <kevin.tassi@gmail.com>
2019-10-19 14:29:54 +0200

    Add basic fetch data

--- src/Nyss.Web/ClientApp/src/components/alert/alert-history-container/AlertHistoryContainer.js
+++ src/Nyss.Web/ClientApp/src/components/alert/alert-history-container/AlertHistoryContainer.js
@@ -4,11 +4,30 @@ import './AlertHistoryContainer.css'
 import AlertHistoryItem from '../alert-history-item/AlertHistoryItem'
 
 class AlertHistoryContainer extends React.Component {
+
+  alertStatus = () => {
+    return true
+  }
+
   render() {
+
+    const items = this.props.data
+    console.log('++', items)
     return (
       <div className="alert-history-container">
         <ul className="alert-container">
-          <AlertHistoryItem />
+          {
+            items.map((item, i) => (
+              // <div>{item.village}</div>
+
+              // const alertStatus = 'd'
+
+              <AlertHistoryItem 
+                status={this.alertStatus} 
+                key={i}
+              />
+            ))
+          }
         </ul>
       </div>
     )
