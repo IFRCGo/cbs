686b0d911a064e11ac3b87d172330cb3a0a1e05c
Kevin <kevin.tassi@gmail.com>
2019-10-19 12:23:42 +0200

    Create a view for alert history

---
+++ src/Nyss.Web/ClientApp/src/views/AlertHistoryView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AlertHistoryView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Alert history:</h1>
+      </div>
+    )
+  }
+}
+
+export default AlertHistoryView;
